05048777027a7a36fe85da4a578a457386079cc7
Mike Ng <mike.ng@optimizely.com>
2016-11-14 10:53:16 -0800

    Add global variable export for use in non CommonJS environments.
    Summary: This is needed for us to set up in Bower and have the SDK pulled in as a snippet.
    
    Test Plan: Unit and manual
    
    Reviewers: #oasis_team_review
    
    Differential Revision: https://phabricator.optimizely.com/D13788

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,6 @@
 var webpack = require('webpack');
 
 module.exports = {
-  module: {
-    loaders: [
-      { test: /\.json/, loader: 'json-loader' },
-    ],
-  },
   plugins: [
     new webpack.optimize.UglifyJsPlugin({
       compress: {
@@ -17,9 +12,8 @@ module.exports = {
     }),
     new webpack.optimize.DedupePlugin(),
   ],
-  entry: './index.js',
   output: {
-    filename: 'optimizely.min.js',
-    path: './dist'
+    library: 'optimizelyClient',
+    libraryTarget: 'umd'
   },
 };
