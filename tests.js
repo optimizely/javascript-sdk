e5a631db0eaa2aad61698a18ae4bb95c118bd3e6
Caitlin <crubin@us-crubin.local>
2017-05-23 17:09:16 -0700

    added tests for event_dispatcher

--- tests.js
+++ tests.js
@@ -18,6 +18,8 @@ var enums = require('optimizely-server-sdk/lib/utils/enums');
 var Optimizely = require('optimizely-server-sdk/lib/optimizely');
 var optimizelyFactory = require('./');
 var packageJSON = require('./package.json');
+var eventDispatcher = require('./lib/plugins/event_dispatcher');
+var testData = require('./tests/test_data');
 
 var chai = require('chai');
 var assert = chai.assert;
@@ -88,6 +90,20 @@ describe('javascript-sdk', function() {
 
         assert.equal(optlyInstance.logger.logLevel, enums.LOG_LEVEL.INFO);
       });
+
+      it('should activate with provided event dispatcher', function(done) {
+        var optlyInstance = optimizelyFactory.createInstance({
+          datafile: testData.getTestProjectConfig(),
+          errorHandler: fakeErrorHandler,
+          eventDispatcher: eventDispatcher,
+          logger: fakeLogger,
+        });
+        var activate = optlyInstance.activate('testExperiment', 'testUser');
+        done();
+        // console.log("Activate response: ", activate);
+        // assert.equal(optlyInstance.logger.logLevel, enums.LOG_LEVEL.INFO);
+      });
+
     });
   });
 });
