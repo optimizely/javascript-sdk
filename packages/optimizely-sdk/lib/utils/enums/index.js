e87596eefcd03bb46b1df03ad195f2c6a7a8609b
Matt Carroll <matt.carroll@optimizely.com>
2018-10-22 10:26:37 -0700

    Release 3.0.0-alpha1

--- packages/optimizely-sdk/lib/utils/enums/index.js
+++ packages/optimizely-sdk/lib/utils/enums/index.js
@@ -140,7 +140,7 @@ exports.CONTROL_ATTRIBUTES = {
 
 exports.JAVASCRIPT_CLIENT_ENGINE = 'javascript-sdk';
 exports.NODE_CLIENT_ENGINE = 'node-sdk';
-exports.NODE_CLIENT_VERSION = '2.2.0';
+exports.NODE_CLIENT_VERSION = '3.0.0-alpha1';
 
 /*
  * Notification types for use with NotificationCenter
