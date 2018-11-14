92326272492a656c185156563c2a5b987821b214
Mike Ng <mike.ng@optimizely.com>
2018-11-14 13:40:15 -0800

    fix(bundling): Publish the unminified UMDbundle along with the minified one.

--- packages/optimizely-sdk/scripts/build_browser_umd.sh
+++ packages/optimizely-sdk/scripts/build_browser_umd.sh
@@ -26,3 +26,28 @@ Object.defineProperty(window, 'optimizelyClient', {
 });
 EOF
 
+# Builds the unminified bundle
+npx webpack lib/index.browser.js dist/optimizely.browser.umd.js \
+  --define process.env.NODE_ENV="production" \
+  --output-library-target=umd \
+  --output-library="$BUNDLE_NAME"
+
+# Append some backwards-compatibility code to the bundle
+cat - >> dist/optimizely.browser.umd.min.js <<EOF
+
+
+window._optimizelyClientWarningGiven = false;
+
+Object.defineProperty(window, 'optimizelyClient', {
+  get: function () {
+    if (!window._optimizelyClientWarningGiven) {
+      console.warn('Accessing the SDK via window.optimizelyClient is deprecated; please use window.optimizelySdk instead. This alias will be dropped in 3.0.0.');
+      window._optimizelyClientWarningGiven = true;
+    }
+
+    return {
+      createInstance: window.optimizelySdk.createInstance
+    };
+  }
+});
+EOF
