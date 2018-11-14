17aaf1f7b2080943e502865103266829ef90802a
Mike Ng <mike.ng@optimizely.com>
2018-11-14 14:10:38 -0800

    chore: Prepare for 2.3.1 release.

--- packages/optimizely-sdk/lib/utils/enums/index.js
+++ packages/optimizely-sdk/lib/utils/enums/index.js
@@ -141,7 +141,7 @@ exports.CONTROL_ATTRIBUTES = {
 
 exports.JAVASCRIPT_CLIENT_ENGINE = 'javascript-sdk';
 exports.NODE_CLIENT_ENGINE = 'node-sdk';
-exports.NODE_CLIENT_VERSION = '2.3.0';
+exports.NODE_CLIENT_VERSION = '2.3.1';
 
 /*
  * Notification types for use with NotificationCenter
