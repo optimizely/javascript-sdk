32f54af0dabf88614cde0978f2e9e1ab7be9b0d2
jordangarcia <jordan.garcia@gmail.com>
2018-11-16 20:33:47 -0800

    Implment in React example

--- packages/js-web-sdk/packages/react-example-ts/src/App.tsx
+++ packages/js-web-sdk/packages/react-example-ts/src/App.tsx
@@ -6,20 +6,18 @@ import Example from './Example'
 
 import {
   OptimizelyFeature,
-  OptimizelyFeatureVariables,
   OptimizelyProvider,
-  OptimizelyExperimentSwitch,
-  OptimizelyMatch,
-  OptimizelyFeatureVariableSwitch,
+  OptimizelyExperiment,
 } from '@optimizely/react-sdk'
 
 import {
   VariableValuesObject,
   OptimizelyDatafile,
+  OptimizelySDKWrapper,
 } from '@optimizely/js-sdk-wrapper'
 
 interface AppProps {
-  datafile: OptimizelyDatafile,
+  optimizely: OptimizelySDKWrapper,
 }
 
 class App extends React.Component<AppProps> {
@@ -28,101 +26,33 @@ class App extends React.Component<AppProps> {
   }
 
   render() {
-    const { datafile } = this.props
+    const { optimizely } = this.props
 
     return (
-      <OptimizelyProvider datafile={datafile} userId="jordan">
+      <OptimizelyProvider optimizely={optimizely} timeout={5}>
         <div className="App">
           <Example title="Experiment Example">
-            <OptimizelyExperimentSwitch experiment="abtest1">
-              <OptimizelyMatch value="var1">Variation 1</OptimizelyMatch>
-              <OptimizelyMatch value="var2">Variation 2</OptimizelyMatch>
-            </OptimizelyExperimentSwitch>
-          </Example>
+            <p>
+              <OptimizelyExperiment experiment="abtest1">
+                {(variation) => {
+                  if (variation === 'var1') {
+                    return "var1"
+                  } else if (variation === 'var2') {
+                    return "var2"
+                  } else {
+                    return "default"
+                  }
+                }}
+              </OptimizelyExperiment>
+            </p>
 
-          <Example title="Experiment Example 2">
-            <OptimizelyExperimentSwitch experiment="abtest1">
-              <OptimizelyMatch value="var1" render={() => <h1>variation 1</h1>} />
-              <OptimizelyMatch default render={() => <h1>variation 2 (default)</h1>} />
-            </OptimizelyExperimentSwitch>
-          </Example>
-
-          <Example title="Feature example">
-            <OptimizelyFeature
-              feature="feature1"
-              renderEnabled={(featureVariables: VariableValuesObject) => (
-                <div>
-                  "feature1" is enabled
-                  <pre>{JSON.stringify(featureVariables, null, '  ')}</pre>
-                </div>
-              )}
-              renderDisabled={() => `Feature1 is disabled`}
-            />
-          </Example>
-
-          <Example title="FeatureVariableSwitch example">
-            <OptimizelyFeatureVariableSwitch feature="feature1" variable="header">
-              <OptimizelyMatch
-                value="Hi Jess!"
-                render={variableValues => <p>{variableValues.content}</p>}
-              />
-              <OptimizelyMatch default render={() => <p>variation 2 (default)</p>} />
-            </OptimizelyFeatureVariableSwitch>
-          </Example>
-
-          <Example title="FeatureVariableSwitch with matcher example">
-            <OptimizelyFeatureVariableSwitch
-              feature="feature1"
-              variable="variation"
-              matcher={(variableVal, matchVal) =>
-                (matchVal as Array<string>).indexOf(variableVal as string) > -1
-              }
-            >
-              <OptimizelyMatch
-                value={['jordan', 'jess']}
-                render={variableValues => (
-                  <p>(1) Got variation {variableValues.variation}</p>
+            <p>
+              <OptimizelyFeature feature="feature1">
+                {(isEnabled, variables) => (
+                  isEnabled ? 'is enabled' : 'is disabled'
                 )}
-              />
-              <OptimizelyMatch
-                value={['jess']}
-                render={variableValues => (
-                  <p>(2) Got variation {variableValues.variation}</p>
-                )}
-              />
-              <OptimizelyMatch
-                default
-                render={variableValues => (
-                  <p>(default) Got variation {variableValues.variation}</p>
-                )}
-              />
-            </OptimizelyFeatureVariableSwitch>
-          </Example>
-
-          <Example title="FeatureVariables (render prop) example">
-            <OptimizelyFeatureVariables
-              feature="feature1"
-              render={variables => <pre>{JSON.stringify(variables, null, '  ')}</pre>}
-            />
-          </Example>
-
-          <Example title="FeatureVariables (children render) example">
-            <OptimizelyFeatureVariables feature="feature1">
-              {({ header, content }) => (
-                <>
-                  <h4>{header || 'default'}</h4>
-                  <p>{content}</p>
-                </>
-              )}
-            </OptimizelyFeatureVariables>
-          </Example>
-
-          <Example title="FeatureVariables (single variable) example">
-            <h4>
-              <OptimizelyFeatureVariables feature="feature1">
-                {({ header }) => header || 'default'}
-              </OptimizelyFeatureVariables>
-            </h4>
+              </OptimizelyFeature>
+            </p>
           </Example>
         </div>
       </OptimizelyProvider>
--- packages/js-web-sdk/packages/react-example-ts/src/index.tsx
+++ packages/js-web-sdk/packages/react-example-ts/src/index.tsx
@@ -1,13 +1,17 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
+import { OptimizelySDKWrapper } from '@optimizely/js-sdk-wrapper';
 
 ;(async function () {
-  const resp = await fetch('https://optimizely.s3.amazonaws.com/datafiles/BsSyVRsUbE3ExgGCJ9w1to.json', { mode: 'cors' });
-  let datafile: any = await resp.json();
+  const optimizely = new OptimizelySDKWrapper({
+    userId: 'jordan',
+    datafileUrl: 'https://optimizely.s3.amazonaws.com/datafiles/BsSyVRsUbE3ExgGCJ9w1to.json',
+  })
+
 
   ReactDOM.render(
-    <App datafile={datafile} />,
+    <App optimizely={optimizely} />,
     document.getElementById('root') as HTMLElement
   );
 })()
