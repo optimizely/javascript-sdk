a3bbcc7a05a9880e185bfb4754b1bde588823597
Caitlin Rubin <caitlin.rubin@optimizely.com>
2018-11-20 14:18:10 -0800

    added back other browsers

--- packages/optimizely-sdk/karma.umd.conf.js
+++ packages/optimizely-sdk/karma.umd.conf.js
@@ -34,10 +34,54 @@ module.exports = function(config) {
         browser_version: '21.0',
         os: 'OS X',
         os_version: 'Mountain Lion'
+      },
+      bs_edge: {
+        base: 'BrowserStack',
+        os: 'Windows',
+        os_version: '10',
+        browser: 'edge',
+        device: null,
+        browser_version: '15.0'
+      },
+      bs_firefox_mac: {
+        base: 'BrowserStack',
+        browser: 'firefox',
+        browser_version: '21.0',
+        os: 'OS X',
+        os_version: 'Mountain Lion'
+      },
+      bs_ie: {
+        base: 'BrowserStack',
+        os: 'Windows',
+        os_version: '7',
+        browser: 'ie',
+        device: null,
+        browser_version: '10.0'
+      },
+      bs_iphone6: {
+        base: 'BrowserStack',
+        device: 'iPhone 6',
+        os: 'ios',
+        os_version: '8.3'
+      },
+      bs_opera_mac: {
+        base: 'BrowserStack',
+        browser: 'opera',
+        browser_version: '37',
+        os: 'OS X',
+        os_version: 'Mountain Lion'
+      },
+      bs_safari: {
+        base: 'BrowserStack',
+        os: 'OS X',
+        os_version: 'Mountain Lion',
+        browser: 'safari',
+        device: null,
+        browser_version: '6.2'
       }
     },
 
-    browsers: ['bs_chrome_mac'],
+    browsers: ['bs_chrome_mac', 'bs_edge', 'bs_firefox_mac', 'bs_ie', 'bs_iphone6', 'bs_opera_mac', 'bs_safari'],
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
