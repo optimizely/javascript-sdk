bec3b3f1a8a17e05131e7d5794b4156eed8e0832
Matt Carroll <mjc1283@users.noreply.github.com>
2019-01-24 11:07:10 -0800

    Build scripts for js-web-sdk (#7)

---
+++ packages/js-web-sdk/packages/js-web-sdk/scripts/config.js
@@ -0,0 +1,47 @@
+const typescript = require("rollup-plugin-typescript2");
+const commonjs = require("rollup-plugin-commonjs");
+const replace = require("rollup-plugin-replace");
+const resolve = require("rollup-plugin-node-resolve");
+const { uglify } = require("rollup-plugin-uglify");
+
+const packageDeps = require("../package.json").dependencies || {};
+const packagePeers = require("../package.json").peerDependencies || {};
+
+function getExternals(externals) {
+  return externals === "peers"
+    ? Object.keys(packagePeers)
+    : Object.keys(packageDeps).concat(Object.keys(packagePeers));
+}
+
+function getPlugins(env) {
+  const plugins = [resolve({ browser: true })];
+
+  if (env) {
+    plugins.push(
+      replace({
+        "process.env.NODE_ENV": JSON.stringify(env)
+      })
+    );
+  }
+
+  plugins.push(
+    typescript(),
+    commonjs({
+      include: /node_modules/
+    })
+  );
+
+  if (env === "production") {
+    plugins.push(uglify());
+  }
+
+  return plugins;
+}
+
+const config = {
+  input: "src/index.ts",
+  external: getExternals(process.env.EXTERNALS),
+  plugins: getPlugins(process.env.BUILD_ENV)
+};
+
+module.exports = config;
