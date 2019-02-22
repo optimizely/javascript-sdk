46e5cf5d9084dafd74aa2e6d2130ee20b2a764f8
jordangarcia <jordan.garcia@gmail.com>
2019-02-21 17:49:52 -0800

    Fix broken reference

--- packages/optimizely-sdk/lib/plugins/logger/enums.js
+++ packages/optimizely-sdk/lib/plugins/logger/enums.js
@@ -13,4 +13,4 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-exports.LOG_LEVEL = require('@optimizely/js-sdk-core').LogLevel;
+exports.LOG_LEVEL = require('@optimizely/js-sdk-logging').LogLevel;
