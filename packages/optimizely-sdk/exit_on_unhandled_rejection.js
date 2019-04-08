90cdd8746fe7abab800b0ef439beb7ba45f4f986
Matt Carroll <matt.carroll@optimizely.com>
2019-04-08 13:20:59 -0700

    Add explanation comment

--- packages/optimizely-sdk/exit_on_unhandled_rejection.js
+++ packages/optimizely-sdk/exit_on_unhandled_rejection.js
@@ -1,3 +1,7 @@
+/*
+ * This is to stop & fail tests when an unhandled promise rejection occurs.
+ * See: https://nodejs.org/api/process.html#process_event_unhandledrejection
+ */
 process.on('unhandledRejection', function(err) {
   console.error('Unhandled promise rejection');
   if (err) {
