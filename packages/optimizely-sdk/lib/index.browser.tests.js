4845e701696f045f83da8916f0fca343f83e45ce
Matt Carroll <matt.carroll@optimizely.com>
2019-04-26 10:17:04 -0700

    Update version number in tests

--- packages/optimizely-sdk/lib/index.browser.tests.js
+++ packages/optimizely-sdk/lib/index.browser.tests.js
@@ -119,7 +119,7 @@ describe('javascript-sdk', function() {
         optlyInstance.onReady().catch(function() {});
 
         assert.instanceOf(optlyInstance, Optimizely);
-        assert.equal(optlyInstance.clientVersion, '3.1.0');
+        assert.equal(optlyInstance.clientVersion, '3.2.0-alpha');
       });
 
       it('should set the JavaScript client engine and version', function() {
