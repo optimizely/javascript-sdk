db57438268f7f1c7b197e69395ea722f726dd27a
Matt Carroll <mjc1283@users.noreply.github.com>
2019-05-30 13:28:32 -0700

    Prepare for 3.2.0 release (#282)

--- packages/optimizely-sdk/lib/index.browser.tests.js
+++ packages/optimizely-sdk/lib/index.browser.tests.js
@@ -119,7 +119,7 @@ describe('javascript-sdk', function() {
         optlyInstance.onReady().catch(function() {});
 
         assert.instanceOf(optlyInstance, Optimizely);
-        assert.equal(optlyInstance.clientVersion, '3.2.0-beta');
+        assert.equal(optlyInstance.clientVersion, '3.2.0');
       });
 
       it('should set the JavaScript client engine and version', function() {
