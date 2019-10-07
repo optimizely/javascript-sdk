2f3e7c421aaeff4f65ac24f09ad05a3077e6251b
Zeeshan Ashraf <zashraf@folio3.com>
2019-10-07 19:24:24 +0500

    made react native entry point independent of browser entry

--- packages/optimizely-sdk/lib/index.react_native.js
+++ packages/optimizely-sdk/lib/index.react_native.js
@@ -13,14 +13,34 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-var Logger = require('./plugins/logger/index.react_native');
-var browserIndex = require('./index.browser');
+var logging = require('@optimizely/js-sdk-logging');
+var fns = require('./utils/fns');
+var configValidator = require('./utils/config_validator');
+var defaultErrorHandler = require('./plugins/error_handler');
+var defaultEventDispatcher = require('./plugins/event_dispatcher/index.browser');
+var enums = require('./utils/enums');
+var loggerPlugin = require('./plugins/logger/index.react_native');
+var Optimizely = require('./optimizely');
+var eventProcessorConfigValidator = require('./utils/event_processor_config_validator');
+
+var logger = logging.getLogger();
+logging.setLogHandler(loggerPlugin.createLogger());
+logging.setLogLevel(logging.LogLevel.INFO);
+
+var DEFAULT_EVENT_BATCH_SIZE = 10;
+var DEFAULT_EVENT_FLUSH_INTERVAL = 1000; // Unit is ms, default is 1s
 
 /**
- * Entry point into the Optimizely Browser SDK for React Native Apps
+ * Entry point into the Optimizely Javascript SDK for React Native
  */
 module.exports = {
-  ...browserIndex,
+  logging: loggerPlugin,
+  errorHandler: defaultErrorHandler,
+  eventDispatcher: defaultEventDispatcher,
+  enums: enums,
+
+  setLogger: logging.setLogHandler,
+  setLogLevel: logging.setLogLevel,
 
   /**
    * Creates an instance of the Optimizely class
@@ -36,9 +56,66 @@ module.exports = {
    * @return {Object} the Optimizely object
    */
   createInstance: function(config) {
-    return browserIndex.createInstance({
-      logger: new Logger(), 
-      ...config,
-    })
-  }
+    try {
+      config = config || {};
+
+      // TODO warn about setting per instance errorHandler / logger / logLevel
+      if (config.errorHandler) {
+        logging.setErrorHandler(config.errorHandler);
+      }
+      if (config.logger) {
+        logging.setLogHandler(config.logger);
+        // respect the logger's shouldLog functionality
+        logging.setLogLevel(logging.LogLevel.NOTSET);
+      }
+      if (config.logLevel !== undefined) {
+        logging.setLogLevel(config.logLevel);
+      }
+
+      try {
+        configValidator.validate(config);
+        config.isValidInstance = true;
+      } catch (ex) {
+        logger.error(ex);
+        config.isValidInstance = false;
+      }
+
+      // Explicitly check for null or undefined
+      // prettier-ignore
+      if (config.skipJSONValidation == null) { // eslint-disable-line eqeqeq
+        config.skipJSONValidation = true;
+      }
+
+      config = fns.assignIn(
+        {
+          clientEngine: enums.JAVASCRIPT_CLIENT_ENGINE,
+          eventBatchSize: DEFAULT_EVENT_BATCH_SIZE,
+          eventFlushInterval: DEFAULT_EVENT_FLUSH_INTERVAL,
+        },
+        config,
+        {
+          eventDispatcher: config.eventDispatcher,
+          // always get the OptimizelyLogger facade from logging
+          logger: logger,
+          errorHandler: logging.getErrorHandler(),
+        }
+      );
+
+      if (!eventProcessorConfigValidator.validateEventBatchSize(config.eventBatchSize)) {
+        logger.warn('Invalid eventBatchSize %s, defaulting to %s', config.eventBatchSize, DEFAULT_EVENT_BATCH_SIZE);
+        config.eventBatchSize = DEFAULT_EVENT_BATCH_SIZE;
+      }
+      if (!eventProcessorConfigValidator.validateEventFlushInterval(config.eventFlushInterval)) {
+        logger.warn('Invalid eventFlushInterval %s, defaulting to %s', config.eventFlushInterval, DEFAULT_EVENT_FLUSH_INTERVAL);
+        config.eventFlushInterval = DEFAULT_EVENT_FLUSH_INTERVAL;
+      }
+
+      var optimizely = new Optimizely(config);
+
+      return optimizely;
+    } catch (e) {
+      logger.error(e);
+      return null;
+    }
+  },
 };
--- packages/optimizely-sdk/lib/plugins/logger/index.react_native.js
+++ packages/optimizely-sdk/lib/plugins/logger/index.react_native.js
@@ -10,12 +10,12 @@ function getLogLevelName(level) {
   }
 }
 
-export default class ReactNativeLogger {
+class ReactNativeLogger {
   log(level, message) {
     const formattedMessage = `[OPTIMIZELY] - ${getLogLevelName(level)} ${new Date().toISOString()} ${message}`;
     switch (level) {
       case LogLevel.INFO: console.info(formattedMessage); break;
-      case LogLevel.ERROR:
+      case LogLevel.ERROR: 
       case LogLevel.WARNING: console.warn(formattedMessage); break;
       case LogLevel.DEBUG:
       case LogLevel.NOTSET: console.log(formattedMessage); break;
@@ -23,4 +23,16 @@ export default class ReactNativeLogger {
   }
 }
 
-module.exports = ReactNativeLogger;
+function NoOpLogger() {}
+
+NoOpLogger.prototype.log = function() {};
+
+module.exports = {
+  createLogger: function() {
+    return new ReactNativeLogger();
+  },
+
+  createNoOpLogger: function() {
+    return new NoOpLogger();
+  },
+};
