b38391344abca54a8941b817059657d4d429a0c1
Matt Carroll <matt.carroll@optimizely.com>
2019-12-17 15:24:15 -0800

    Format config file, add Promise global, disable es6

--- packages/optimizely-sdk/.eslintrc.js
+++ packages/optimizely-sdk/.eslintrc.js
@@ -1,27 +1,27 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es6": true,
-        "node": true,
+  env: {
+    browser: true,
+    commonjs: true,
+    node: true,
+  },
+  extends: 'eslint:recommended',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    Promise: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 5,
+  },
+  rules: {
+    'no-prototype-builtins': 'off',
+  },
+  overrides: [
+    {
+      files: ['*.tests.js'],
+      env: {
+        mocha: true,
+      },
     },
-    "extends": "eslint:recommended",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 5
-    },
-    "rules": {
-      "no-prototype-builtins": "off"
-    },
-    "overrides": [
-      {
-        "files": ["*.tests.js"],
-        "env": {
-          "mocha": true
-        }
-      }
-    ]
+  ],
 };
