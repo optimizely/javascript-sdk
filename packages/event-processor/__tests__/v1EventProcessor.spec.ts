d582ffa84b40ca112dcd2b1faa937120480fb5f0
Matt Carroll <mjc1283@users.noreply.github.com>
2020-02-18 11:18:28 -0800

    fix (event processor) [OASIS-5907]: stop promise tracks in-flight dispatcher requests (#397)
    Summary:
    
    Add tracking functionality to EventProcessor for every request sent by its dispatcher. This is implemented in a RequestTracker class.
    RequestTracker exposes a method (onRequestsComplete) to get a Promise that fulfills after the completion of requests in-flight at the time of the call.
    stop is updated to return the promise from RequestTracker onRequestsComplete.
    
    Test plan:
    
    - New and existing unit tests
    - Manually tested in browser, Node, & Lambda environments
    
    Issues:
    
    https://optimizely.atlassian.net/browse/OASIS-5907

--- packages/event-processor/__tests__/v1EventProcessor.spec.ts
+++ packages/event-processor/__tests__/v1EventProcessor.spec.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright 2019, Optimizely
+ * Copyright 2019-2020, Optimizely
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -260,6 +260,40 @@ describe('LogTierV1EventProcessor', () => {
       // Since we already stopped the processor, the dispatcher should
       // not have been called again.
       expect(dispatcher.dispatchEvent).toBeCalledTimes(0)
+    })
+
+    it('should resolve the stop promise after all dispatcher requests are done', async () => {
+      const dispatchCbs: Array<EventDispatcherCallback> = []
+      const dispatcher = {
+        dispatchEvent: jest.fn((event: EventV1Request, callback: EventDispatcherCallback) => {
+          dispatchCbs.push(callback)
+        })
+      }
+
+      const processor = new LogTierV1EventProcessor({
+        dispatcher,
+        flushInterval: 100,
+        maxQueueSize: 2,
+      })
+      processor.start()
+
+      for (let i = 0; i < 4; i++) {
+        processor.process(createImpressionEvent())
+      }
+      expect(dispatchCbs.length).toBe(2)
+
+      let stopPromiseResolved = false
+      const stopPromise = processor.stop().then(() => {
+        stopPromiseResolved = true
+      })
+      expect(stopPromiseResolved).toBe(false)
+
+      dispatchCbs[0]({ statusCode: 204 })
+      jest.advanceTimersByTime(100)
+      expect(stopPromiseResolved).toBe(false)
+      dispatchCbs[1]({ statusCode: 204 })
+      await stopPromise
+      expect(stopPromiseResolved).toBe(true)
     })
   })
 
