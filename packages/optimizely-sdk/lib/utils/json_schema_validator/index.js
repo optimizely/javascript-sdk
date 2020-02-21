dc78b0d9aad1e10610fb58e06396a1533f78c188
fayyazarshad <fayyazarshad@folio3.com>
2020-02-21 11:28:49 -0800

    Fixed lint error

--- packages/optimizely-sdk/lib/utils/json_schema_validator/index.js
+++ packages/optimizely-sdk/lib/utils/json_schema_validator/index.js
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-var fns = require('../fns');
 var validate = require('json-schema').validate;
 var sprintf = require('@optimizely/js-sdk-utils').sprintf;
 
