2eef25ca97babef225b8feb2c79a7683a5b96dcf
Zeeshan Ashraf <zashraf@folio3.com>
2020-03-12 12:25:46 -0700

    Added an interface for key val cache and added an implementation for react native async storage

---
+++ packages/datafile-manager/src/persistentKeyValueCache.ts
@@ -0,0 +1,51 @@
+/**
+ * Copyright 2020, Optimizely
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * An Interface to implement a persistent key value cache which supports strings as keys
+ * and string or JSON Object as value.
+ */
+export default interface PersistentKeyValueCache {
+  
+  /**
+   * Returns value stored against a key or null if not found.
+   * @param key 
+   * @returns Promise which resolves with a
+   * 1. String if the value found is a string.
+   * 2. Object if value found was stored as a JSON Object.
+   * 3. null if the key does not exist in the cache.
+   */
+  get(key: string): Promise<string | Object | null>
+
+  /**
+   * Stores value in the persistent cache against a key
+   * @param key 
+   * @param val 
+   */
+  set(key: string, val: string | Object): void
+
+  /**
+   * Checks if a key exists in the cache
+   * @param key 
+   */
+  contains(key: string): Promise<Boolean>
+
+  /**
+   * Removed the key value pair from cache.
+   * @param key 
+   */
+  remove(key: string): void
+}
---
+++ packages/datafile-manager/src/reactNativeAsyncStorageCache.ts
@@ -0,0 +1,52 @@
+/**
+ * Copyright 2020, Optimizely
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import AsyncStorage from '@react-native-community/async-storage';
+import PersistentKeyValueCache from './persistentKeyValueCache';
+
+export default class ReactNativeAsyncStorageCache implements PersistentKeyValueCache {
+  // If value is a valid JSON string, it converts it to object before returning
+  get(key: string): Promise<Object | string | null> {
+    return new Promise((resolve) => {
+      AsyncStorage.getItem(key).then((val: String | null) => {
+        let finalVal = val;
+        if (val) {
+          try {
+            finalVal = JSON.parse(val.valueOf());
+          } catch (ex) {
+            // couldnt parse, its a string
+          }       
+        }
+        resolve(finalVal);
+      });
+    });
+  }
+  
+  // If value is an Object, it stringifies it before storing.
+  set(key: string, val: string | Object): void {
+    AsyncStorage.setItem(key, typeof val === 'object' ? JSON.stringify(val) : val);
+  }
+  
+  contains(key: string): Promise<Boolean> {
+    return new Promise(resolve =>
+      AsyncStorage.getItem(key).then((val: String | null) => resolve(val !== null))
+    )
+  }
+  
+  remove(key: string): void {
+    AsyncStorage.removeItem(key);
+  }
+}
