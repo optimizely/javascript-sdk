7e803e0512377d978afd4aa8c9744ea0fed0273d
Matt Carroll <matt.carroll@optimizely.com>
2020-04-30 12:38:58 -0700

    chore: Prepare for 4.0.0 release

--- packages/optimizely-sdk/lib/index.node.tests.js
+++ packages/optimizely-sdk/lib/index.node.tests.js
@@ -90,7 +90,7 @@ describe('optimizelyFactory', function() {
         optlyInstance.onReady().catch(function() {});
 
         assert.instanceOf(optlyInstance, Optimizely);
-        assert.equal(optlyInstance.clientVersion, '4.0.0-rc.2');
+        assert.equal(optlyInstance.clientVersion, '4.0.0');
       });
 
       describe('event processor configuration', function() {
