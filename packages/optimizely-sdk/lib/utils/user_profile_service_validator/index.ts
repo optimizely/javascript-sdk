664fdb863911af1aa1b1e6c72364be3511121d71
Polina Nguen <polina.nguen@optimizely.com>
2020-07-17 18:30:11 -0700

    Convert user_profile_service_validator module to TS

--- packages/optimizely-sdk/lib/utils/user_profile_service_validator/index.ts
+++ packages/optimizely-sdk/lib/utils/user_profile_service_validator/index.ts
@@ -21,23 +21,20 @@
 import { sprintf } from '@optimizely/js-sdk-utils';
 
 import { ERROR_MESSAGES } from '../enums';
-var MODULE_NAME = 'USER_PROFILE_SERVICE_VALIDATOR';
+
+const MODULE_NAME = 'USER_PROFILE_SERVICE_VALIDATOR';
 
 /**
  * Validates user's provided user profile service instance
- * @param  {Object}  userProfileServiceInstance
- * @return {boolean} True if the instance is valid
+ * @param  {object}  userProfileServiceInstance
+ * @return {boolean} true if the instance is valid
  * @throws If the instance is not valid
  */
-export var validate = function(userProfileServiceInstance) {
+export function validate(userProfileServiceInstance: Record<string, unknown>): boolean {
   if (typeof userProfileServiceInstance.lookup !== 'function') {
     throw new Error(sprintf(ERROR_MESSAGES.INVALID_USER_PROFILE_SERVICE, MODULE_NAME, "Missing function 'lookup'"));
   } else if (typeof userProfileServiceInstance.save !== 'function') {
     throw new Error(sprintf(ERROR_MESSAGES.INVALID_USER_PROFILE_SERVICE, MODULE_NAME, "Missing function 'save'"));
   }
   return true;
-};
-
-export default {
-  validate: validate,
-};
+}
