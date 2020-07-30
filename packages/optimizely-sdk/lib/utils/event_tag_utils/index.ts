0d9ca746aeba6d8afdcc941c218f56f9d3c77e5f
Polina Nguen <polina.nguen@optimizely.com>
2020-07-29 18:16:04 -0700

    Convert event_tag_utils module to TS

--- packages/optimizely-sdk/lib/utils/event_tag_utils/index.ts
+++ packages/optimizely-sdk/lib/utils/event_tag_utils/index.ts
@@ -27,9 +27,9 @@ import {
 /**
  * Provides utility method for parsing event tag values
  */
-var MODULE_NAME = 'EVENT_TAG_UTILS';
-var REVENUE_EVENT_METRIC_NAME = RESERVED_EVENT_KEYWORDS.REVENUE;
-var VALUE_EVENT_METRIC_NAME = RESERVED_EVENT_KEYWORDS.VALUE;
+const MODULE_NAME = 'EVENT_TAG_UTILS';
+const REVENUE_EVENT_METRIC_NAME = RESERVED_EVENT_KEYWORDS.REVENUE;
+const VALUE_EVENT_METRIC_NAME = RESERVED_EVENT_KEYWORDS.VALUE;
 
 /**
  * Grab the revenue value from the event tags. "revenue" is a reserved keyword.
@@ -37,37 +37,38 @@ var VALUE_EVENT_METRIC_NAME = RESERVED_EVENT_KEYWORDS.VALUE;
  * @param {LoggerFacade} logger
  * @return {number|null}
  */
-
 export function getRevenueValue(eventTags: EventTags, logger: LoggerFacade): number | null {
   if (eventTags && eventTags.hasOwnProperty(REVENUE_EVENT_METRIC_NAME)) {
-    var rawValue = eventTags[REVENUE_EVENT_METRIC_NAME];
-    var parsedRevenueValue = parseInt(rawValue as any);
-      if (isNaN(parsedRevenueValue)) {
-        logger.log(LOG_LEVEL.INFO, sprintf(LOG_MESSAGES.FAILED_TO_PARSE_REVENUE, MODULE_NAME, rawValue));
-        return null;
-      }
-      logger.log(LOG_LEVEL.INFO, sprintf(LOG_MESSAGES.PARSED_REVENUE_VALUE, MODULE_NAME, parsedRevenueValue));
-      return parsedRevenueValue;
+    const rawValue = eventTags[REVENUE_EVENT_METRIC_NAME];
+     // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const parsedRevenueValue = parseInt(rawValue as any);
+    if (isNaN(parsedRevenueValue)) {
+      logger.log(LOG_LEVEL.INFO, sprintf(LOG_MESSAGES.FAILED_TO_PARSE_REVENUE, MODULE_NAME, rawValue));
+      return null;
     }
+    logger.log(LOG_LEVEL.INFO, sprintf(LOG_MESSAGES.PARSED_REVENUE_VALUE, MODULE_NAME, parsedRevenueValue));
+    return parsedRevenueValue;
+  }
   return null;
-};
+}
 
 /**
  * Grab the event value from the event tags. "value" is a reserved keyword.
- * @param {object} eventTags
- * @param {object} logger
- * @return {mumber|null}
+ * @param {EventTags} eventTags
+ * @param {LoggerFacade} logger
+ * @return {number|null}
  */
 export function getEventValue(eventTags: EventTags, logger: LoggerFacade): number | null {
   if (eventTags && eventTags.hasOwnProperty(VALUE_EVENT_METRIC_NAME)) {
-    var rawValue = eventTags[VALUE_EVENT_METRIC_NAME];
-      var parsedEventValue = parseFloat(rawValue as any);
-      if (isNaN(parsedEventValue)) {
-        logger.log(LOG_LEVEL.INFO, sprintf(LOG_MESSAGES.FAILED_TO_PARSE_VALUE, MODULE_NAME, rawValue));
-        return null;
-      }
-      logger.log(LOG_LEVEL.INFO, sprintf(LOG_MESSAGES.PARSED_NUMERIC_VALUE, MODULE_NAME, parsedEventValue));
-      return parsedEventValue;
+    const rawValue = eventTags[VALUE_EVENT_METRIC_NAME];
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const parsedEventValue = parseFloat(rawValue as any);
+    if (isNaN(parsedEventValue)) {
+      logger.log(LOG_LEVEL.INFO, sprintf(LOG_MESSAGES.FAILED_TO_PARSE_VALUE, MODULE_NAME, rawValue));
+      return null;
+    }
+    logger.log(LOG_LEVEL.INFO, sprintf(LOG_MESSAGES.PARSED_NUMERIC_VALUE, MODULE_NAME, parsedEventValue));
+    return parsedEventValue;
   }
   return null;
-};
+}
