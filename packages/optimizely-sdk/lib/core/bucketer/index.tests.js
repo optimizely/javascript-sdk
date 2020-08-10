optimizely/javascript-sdk

commit ebc3c64f20990b43428b5246896aff07a04bcce4

fix(logs): Fixing log messages for Targeted Rollouts (#515)

packages/optimizely-sdk/lib/core/bucketer/index.js
@@ -104,7 +104,7 @@ export var bucket = function(bucketerParams) {
   var bucketValue = this._generateBucketValue(bucketingId);
 
   var bucketedUserLogMessage = sprintf(
-    LOG_MESSAGES.USER_ASSIGNED_TO_VARIATION_BUCKET,
+    LOG_MESSAGES.USER_ASSIGNED_TO_EXPERIMENT_BUCKET,
     MODULE_NAME,
     bucketValue,
     bucketerParams.userId
@@ -112,28 +112,11 @@ export var bucket = function(bucketerParams) {
   bucketerParams.logger.log(LOG_LEVEL.DEBUG, bucketedUserLogMessage);
 
   var entityId = this._findBucket(bucketValue, bucketerParams.trafficAllocationConfig);
-  if (!entityId) {
-    var userHasNoVariationLogMessage = sprintf(
-      LOG_MESSAGES.USER_HAS_NO_VARIATION,
-      MODULE_NAME,
-      bucketerParams.userId,
-      bucketerParams.experimentKey
-    );
-    bucketerParams.logger.log(LOG_LEVEL.DEBUG, userHasNoVariationLogMessage);
-  } else if (!bucketerParams.variationIdMap.hasOwnProperty(entityId)) {
+
+  if (!bucketerParams.variationIdMap.hasOwnProperty(entityId)) {
     var invalidVariationIdLogMessage = sprintf(LOG_MESSAGES.INVALID_VARIATION_ID, MODULE_NAME);
     bucketerParams.logger.log(LOG_LEVEL.WARNING, invalidVariationIdLogMessage);
     return null;
-  } else {
-    var variationKey = bucketerParams.variationIdMap[entityId].key;
-    var userInVariationLogMessage = sprintf(
-      LOG_MESSAGES.USER_HAS_VARIATION,
-      MODULE_NAME,
-      bucketerParams.userId,
-      variationKey,
-      bucketerParams.experimentKey
-    );
-    bucketerParams.logger.log(LOG_LEVEL.INFO, userInVariationLogMessage);
   }
 
   return entityId;

packages/optimizely-sdk/lib/core/bucketer/index.tests.js
@@ -75,27 +75,18 @@ describe('lib/core/bucketer', function() {
           expect(bucketer.bucket(bucketerParamsTest1)).to.equal('111128');
 
           var bucketedUser_log1 = createdLogger.log.args[0][1];
-          var bucketedUser_log2 = createdLogger.log.args[1][1];
-
           expect(bucketedUser_log1).to.equal(
-            sprintf(LOG_MESSAGES.USER_ASSIGNED_TO_VARIATION_BUCKET, 'BUCKETER', '50', 'ppid1')
-          );
-          expect(bucketedUser_log2).to.equal(
-            sprintf(LOG_MESSAGES.USER_HAS_VARIATION, 'BUCKETER', 'ppid1', 'control', 'testExperiment')
+            sprintf(LOG_MESSAGES.USER_ASSIGNED_TO_EXPERIMENT_BUCKET, 'BUCKETER', '50', 'ppid1')
           );
 
           var bucketerParamsTest2 = cloneDeep(bucketerParams);
           bucketerParamsTest2.userId = 'ppid2';
           expect(bucketer.bucket(bucketerParamsTest2)).to.equal(null);
 
-          var notBucketedUser_log1 = createdLogger.log.args[2][1];
-          var notBucketedUser_log2 = createdLogger.log.args[3][1];
+          var notBucketedUser_log1 = createdLogger.log.args[1][1];
 
           expect(notBucketedUser_log1).to.equal(
-            sprintf(LOG_MESSAGES.USER_ASSIGNED_TO_VARIATION_BUCKET, 'BUCKETER', '50000', 'ppid2')
-          );
-          expect(notBucketedUser_log2).to.equal(
-            sprintf(LOG_MESSAGES.USER_HAS_NO_VARIATION, 'BUCKETER', 'ppid2', 'testExperiment')
+            sprintf(LOG_MESSAGES.USER_ASSIGNED_TO_EXPERIMENT_BUCKET, 'BUCKETER', '50000', 'ppid2')
           );
         });
       });
@@ -142,7 +133,7 @@ describe('lib/core/bucketer', function() {
             expect(bucketer.bucket(bucketerParams)).to.equal('551');
 
             sinon.assert.calledTwice(bucketerStub);
-            sinon.assert.callCount(createdLogger.log, 4);
+            sinon.assert.callCount(createdLogger.log, 3);
 
             var log1 = createdLogger.log.args[0][1];
             expect(log1).to.equal(
@@ -162,12 +153,7 @@ describe('lib/core/bucketer', function() {
 
             var log3 = createdLogger.log.args[2][1];
             expect(log3).to.equal(
-              sprintf(LOG_MESSAGES.USER_ASSIGNED_TO_VARIATION_BUCKET, 'BUCKETER', '50', 'testUser')
-            );
-
-            var log4 = createdLogger.log.args[3][1];
-            expect(log4).to.equal(
-              sprintf(LOG_MESSAGES.USER_HAS_VARIATION, 'BUCKETER', 'testUser', 'var1exp1', 'groupExperiment1')
+              sprintf(LOG_MESSAGES.USER_ASSIGNED_TO_EXPERIMENT_BUCKET, 'BUCKETER', '50', 'testUser')
             );
           });
 
@@ -258,20 +244,10 @@ describe('lib/core/bucketer', function() {
             expect(bucketer.bucket(bucketerParams)).to.equal('553');
 
             sinon.assert.calledOnce(bucketerStub);
-            sinon.assert.calledTwice(createdLogger.log);
+            sinon.assert.calledOnce(createdLogger.log);
 
             var log1 = createdLogger.log.args[0][1];
-            expect(log1).to.equal(sprintf(LOG_MESSAGES.USER_ASSIGNED_TO_VARIATION_BUCKET, 'BUCKETER', '0', 'testUser'));
-            var log2 = createdLogger.log.args[1][1];
-            expect(log2).to.equal(
-              sprintf(
-                LOG_MESSAGES.USER_HAS_VARIATION,
-                'BUCKETER',
-                'testUser',
-                'overlappingvar1',
-                'overlappingGroupExperiment1'
-              )
-            );
+            expect(log1).to.equal(sprintf(LOG_MESSAGES.USER_ASSIGNED_TO_EXPERIMENT_BUCKET, 'BUCKETER', '0', 'testUser'));
           });
 
           it('should return null when a user does not fall into an experiment within an overlapping group', function() {
@@ -308,7 +284,7 @@ describe('lib/core/bucketer', function() {
         it('should return null', function() {
           var bucketerParamsTest1 = cloneDeep(bucketerParams);
           bucketerParamsTest1.userId = 'ppid1';
-          expect(bucketer.bucket(bucketerParamsTest1)).to.equal('');
+          expect(bucketer.bucket(bucketerParamsTest1)).to.equal(null);
         });
       });
 

packages/optimizely-sdk/lib/core/decision_service/index.js
@@ -27,6 +27,7 @@ var ERROR_MESSAGES = enums.ERROR_MESSAGES;
 var LOG_LEVEL = enums.LOG_LEVEL;
 var LOG_MESSAGES = enums.LOG_MESSAGES;
 var DECISION_SOURCES = enums.DECISION_SOURCES;
+var AUDIENCE_EVALUATION_TYPES = enums.AUDIENCE_EVALUATION_TYPES;
 
 /**
  * Optimizely's decision service that determines which variation of an experiment the user will be allocated to.
@@ -90,7 +91,14 @@ DecisionService.prototype.getVariation = function(configObj, experimentKey, user
   }
 
   // Perform regular targeting and bucketing
-  if (!this.__checkIfUserIsInAudience(configObj, experimentKey, userId, attributes)) {
+  if (!this.__checkIfUserIsInAudience(configObj, experimentKey, AUDIENCE_EVALUATION_TYPES.EXPERIMENT, userId, attributes, '')) {
+    var userDoesNotMeetConditionsLogMessage = sprintf(
+      LOG_MESSAGES.USER_NOT_IN_EXPERIMENT,
+      MODULE_NAME,
+      userId,
+      experimentKey
+    );
+    this.logger.log(LOG_LEVEL.INFO, userDoesNotMeetConditionsLogMessage);
     return null;
   }
 
@@ -98,9 +106,24 @@ DecisionService.prototype.getVariation = function(configObj, experimentKey, user
   var variationId = bucketer.bucket(bucketerParams);
   variation = configObj.variationIdMap[variationId];
   if (!variation) {
+    var userHasNoVariationLogMessage = sprintf(
+      LOG_MESSAGES.USER_HAS_NO_VARIATION,
+      MODULE_NAME,
+      userId,
+      experimentKey
+    );
+    this.logger.log(LOG_LEVEL.DEBUG, userHasNoVariationLogMessage);
     return null;
   }
 
+  var userInVariationLogMessage = sprintf(
+    LOG_MESSAGES.USER_HAS_VARIATION,
+    MODULE_NAME,
+    userId,
+    variation.key,
+    experimentKey
+  );
+  this.logger.log(LOG_LEVEL.INFO, userInVariationLogMessage);
   // persist bucketing
   this.__saveUserProfile(experiment, variation, userId, experimentBucketMap);
 
@@ -171,13 +194,15 @@ DecisionService.prototype.__getWhitelistedVariation = function(experiment, userI
 
 /**
  * Checks whether the user is included in experiment audience
- * @param  {Object}  configObj     The parsed project configuration object
- * @param  {string}  experimentKey Key of experiment being validated
- * @param  {string}  userId        ID of user
- * @param  {Object}  attributes    Optional parameter for user's attributes
+ * @param  {Object}  configObj            The parsed project configuration object
+ * @param  {string}  experimentKey        Key of experiment being validated
+ * @param  {string}  evaluationAttribute  String representing experiment key or rule
+ * @param  {string}  userId               ID of user
+ * @param  {Object}  attributes           Optional parameter for user's attributes
+ * @param  {string}  loggingKey           String representing experiment key or rollout rule. To be used in log messages only.
  * @return {boolean} True if user meets audience conditions
  */
-DecisionService.prototype.__checkIfUserIsInAudience = function(configObj, experimentKey, userId, attributes) {
+DecisionService.prototype.__checkIfUserIsInAudience = function(configObj, experimentKey, evaluationAttribute, userId, attributes, loggingKey) {
   var experimentAudienceConditions = projectConfig.getExperimentAudienceConditions(configObj, experimentKey);
   var audiencesById = projectConfig.getAudiencesById(configObj);
   this.logger.log(
@@ -185,7 +210,8 @@ DecisionService.prototype.__checkIfUserIsInAudience = function(configObj, experi
     sprintf(
       LOG_MESSAGES.EVALUATING_AUDIENCES_COMBINED,
       MODULE_NAME,
-      experimentKey,
+      evaluationAttribute,
+      loggingKey || experimentKey,
       JSON.stringify(experimentAudienceConditions)
     )
   );
@@ -195,23 +221,13 @@ DecisionService.prototype.__checkIfUserIsInAudience = function(configObj, experi
     sprintf(
       LOG_MESSAGES.AUDIENCE_EVALUATION_RESULT_COMBINED,
       MODULE_NAME,
-      experimentKey,
+      evaluationAttribute,
+      loggingKey || experimentKey,
       result.toString().toUpperCase()
     )
   );
 
-  if (!result) {
-    var userDoesNotMeetConditionsLogMessage = sprintf(
-      LOG_MESSAGES.USER_NOT_IN_EXPERIMENT,
-      MODULE_NAME,
-      userId,
-      experimentKey
-    );
-    this.logger.log(LOG_LEVEL.INFO, userDoesNotMeetConditionsLogMessage);
-    return false;
-  }
-
-  return true;
+  return result;
 };
 
 /**
@@ -335,24 +351,8 @@ DecisionService.prototype.__saveUserProfile = function(experiment, variation, us
 DecisionService.prototype.getVariationForFeature = function(configObj, feature, userId, attributes) {
   var experimentDecision = this._getVariationForFeatureExperiment(configObj, feature, userId, attributes);
   if (experimentDecision.variation !== null) {
-    this.logger.log(
-      LOG_LEVEL.DEBUG,
-      sprintf(
-        LOG_MESSAGES.USER_IN_FEATURE_EXPERIMENT,
-        MODULE_NAME,
-        userId,
-        experimentDecision.variation.key,
-        experimentDecision.experiment.key,
-        feature.key
-      )
-    );
     return experimentDecision;
   }
-
-  this.logger.log(
-    LOG_LEVEL.DEBUG,
-    sprintf(LOG_MESSAGES.USER_NOT_IN_FEATURE_EXPERIMENT, MODULE_NAME, userId, feature.key)
-  );
 
   var rolloutDecision = this._getVariationForRollout(configObj, feature, userId, attributes);
   if (rolloutDecision.variation !== null) {
@@ -456,50 +456,56 @@ DecisionService.prototype._getVariationForRollout = function(configObj, feature,
   // "everyone else", which will be evaluated separately outside this loop
   var endIndex = rollout.experiments.length - 1;
   var index;
-  var experiment;
+  var rolloutRule;
   var bucketerParams;
   var variationId;
   var variation;
+  var loggingKey;
   for (index = 0; index < endIndex; index++) {
-    experiment = configObj.experimentKeyMap[rollout.experiments[index].key];
+    rolloutRule = configObj.experimentKeyMap[rollout.experiments[index].key];
+    loggingKey = index + 1;
 
-    if (!this.__checkIfUserIsInAudience(configObj, experiment.key, userId, attributes)) {
+    if (!this.__checkIfUserIsInAudience(configObj, rolloutRule.key, AUDIENCE_EVALUATION_TYPES.RULE, userId, attributes, loggingKey)) {
       this.logger.log(
         LOG_LEVEL.DEBUG,
-        sprintf(LOG_MESSAGES.USER_DOESNT_MEET_CONDITIONS_FOR_TARGETING_RULE, MODULE_NAME, userId, index + 1)
+        sprintf(LOG_MESSAGES.USER_DOESNT_MEET_CONDITIONS_FOR_TARGETING_RULE, MODULE_NAME, userId, loggingKey)
       );
       continue;
     }
 
     this.logger.log(
       LOG_LEVEL.DEBUG,
-      sprintf(LOG_MESSAGES.USER_MEETS_CONDITIONS_FOR_TARGETING_RULE, MODULE_NAME, userId, index + 1)
+      sprintf(LOG_MESSAGES.USER_MEETS_CONDITIONS_FOR_TARGETING_RULE, MODULE_NAME, userId, loggingKey)
     );
-    bucketerParams = this.__buildBucketerParams(configObj, experiment.key, bucketingId, userId);
+    bucketerParams = this.__buildBucketerParams(configObj, rolloutRule.key, bucketingId, userId);
     variationId = bucketer.bucket(bucketerParams);
     variation = configObj.variationIdMap[variationId];
     if (variation) {
       this.logger.log(
         LOG_LEVEL.DEBUG,
-        sprintf(LOG_MESSAGES.USER_BUCKETED_INTO_TARGETING_RULE, MODULE_NAME, userId, index + 1)
+        sprintf(LOG_MESSAGES.USER_BUCKETED_INTO_TARGETING_RULE, MODULE_NAME, userId, loggingKey)
       );
       return {
-        experiment: experiment,
+        experiment: rolloutRule,
         variation: variation,
         decisionSource: DECISION_SOURCES.ROLLOUT,
       };
     } else {
       this.logger.log(
         LOG_LEVEL.DEBUG,
-        sprintf(LOG_MESSAGES.USER_NOT_BUCKETED_INTO_TARGETING_RULE, MODULE_NAME, userId, index + 1)
+        sprintf(LOG_MESSAGES.USER_NOT_BUCKETED_INTO_TARGETING_RULE, MODULE_NAME, userId, loggingKey)
       );
       break;
     }
   }
 
-  var everyoneElseExperiment = configObj.experimentKeyMap[rollout.experiments[endIndex].key];
-  if (this.__checkIfUserIsInAudience(configObj, everyoneElseExperiment.key, userId, attributes)) {
-    bucketerParams = this.__buildBucketerParams(configObj, everyoneElseExperiment.key, bucketingId, userId);
+  var everyoneElseRule = configObj.experimentKeyMap[rollout.experiments[endIndex].key];
+  if (this.__checkIfUserIsInAudience(configObj, everyoneElseRule.key, AUDIENCE_EVALUATION_TYPES.RULE, userId, attributes, 'Everyone Else')) {
+    this.logger.log(
+      LOG_LEVEL.DEBUG,
+      sprintf(LOG_MESSAGES.USER_MEETS_CONDITIONS_FOR_TARGETING_RULE, MODULE_NAME, userId, 'Everyone Else')
+    );
+    bucketerParams = this.__buildBucketerParams(configObj, everyoneElseRule.key, bucketingId, userId);
     variationId = bucketer.bucket(bucketerParams);
     variation = configObj.variationIdMap[variationId];
     if (variation) {
@@ -508,7 +514,7 @@ DecisionService.prototype._getVariationForRollout = function(configObj, feature,
         sprintf(LOG_MESSAGES.USER_BUCKETED_INTO_EVERYONE_TARGETING_RULE, MODULE_NAME, userId)
       );
       return {
-        experiment: everyoneElseExperiment,
+        experiment: everyoneElseRule,
         variation: variation,
         decisionSource: DECISION_SOURCES.ROLLOUT,
       };

packages/optimizely-sdk/lib/core/decision_service/index.tests.js
@@ -39,7 +39,7 @@ import {
 } from '../../tests/test_data';
 
 var testData = getTestProjectConfig();
-var testDataWithFeatures = getTestProjectConfigWithFeatures(); 
+var testDataWithFeatures = getTestProjectConfigWithFeatures();
 
 describe('lib/core/decision_service', function() {
   describe('APIs', function() {
@@ -285,7 +285,7 @@ describe('lib/core/decision_service', function() {
           );
           sinon.assert.calledWith(userProfileLookupStub, 'decision_service_user');
           sinon.assert.calledOnce(bucketerStub);
-          assert.strictEqual(4, mockLogger.log.callCount);
+          assert.strictEqual(5, mockLogger.log.callCount);
           sinon.assert.calledWith(userProfileServiceInstance.save, {
             user_id: 'decision_service_user',
             experiment_bucket_map: {
@@ -299,7 +299,7 @@ describe('lib/core/decision_service', function() {
             'DECISION_SERVICE: User decision_service_user is not in the forced variation map.'
           );
           assert.strictEqual(
-            mockLogger.log.args[3][1],
+            mockLogger.log.args[4][1],
             'DECISION_SERVICE: Saved variation "control" of experiment "testExperiment" for user "decision_service_user".'
           );
         });
@@ -336,13 +336,13 @@ describe('lib/core/decision_service', function() {
           sinon.assert.calledWith(userProfileLookupStub, 'decision_service_user');
           sinon.assert.calledOnce(bucketerStub); // should still go through with bucketing
 
-          assert.strictEqual(4, mockLogger.log.callCount);
+          assert.strictEqual(5, mockLogger.log.callCount);
           assert.strictEqual(
             mockLogger.log.args[0][1],
             'DECISION_SERVICE: User decision_service_user is not in the forced variation map.'
           );
           assert.strictEqual(
-            mockLogger.log.args[3][1],
+            mockLogger.log.args[4][1],
             'DECISION_SERVICE: Error while saving user profile for user ID "decision_service_user": I am an error.'
           );
 
@@ -560,9 +560,14 @@ describe('lib/core/decision_service', function() {
 
       it('should return true when audience conditions are met', function() {
         assert.isTrue(
-          decisionServiceInstance.__checkIfUserIsInAudience(configObj, 'testExperimentWithAudiences', 'testUser', {
-            browser_type: 'firefox',
-          })
+          decisionServiceInstance.__checkIfUserIsInAudience(
+            configObj,
+            'testExperimentWithAudiences',
+            "experiment",
+            'testUser',
+            { browser_type: 'firefox' },
+            ''
+          )
         );
         assert.strictEqual(2, mockLogger.log.callCount);
         assert.strictEqual(
@@ -576,7 +581,16 @@ describe('lib/core/decision_service', function() {
       });
 
       it('should return true when experiment has no audience', function() {
-        assert.isTrue(decisionServiceInstance.__checkIfUserIsInAudience(configObj, 'testExperiment', 'testUser'));
+        assert.isTrue(
+          decisionServiceInstance.__checkIfUserIsInAudience(
+            configObj,
+            'testExperiment',
+            "experiment",
+            'testUser',
+            {},
+            ''
+          )
+        );
         assert.isTrue(__audienceEvaluateSpy.alwaysReturned(true));
 
         assert.strictEqual(2, mockLogger.log.callCount);
@@ -592,11 +606,18 @@ describe('lib/core/decision_service', function() {
 
       it('should return false when audience conditions can not be evaluated', function() {
         assert.isFalse(
-          decisionServiceInstance.__checkIfUserIsInAudience(configObj, 'testExperimentWithAudiences', 'testUser')
+          decisionServiceInstance.__checkIfUserIsInAudience(
+            configObj,
+            'testExperimentWithAudiences',
+            "experiment",
+            'testUser',
+            {},
+            ''
+          )
         );
         assert.isTrue(__audienceEvaluateSpy.alwaysReturned(false));
 
-        assert.strictEqual(3, mockLogger.log.callCount);
+        assert.strictEqual(2, mockLogger.log.callCount);
         assert.strictEqual(
           mockLogger.log.args[0][1],
           'DECISION_SERVICE: Evaluating audiences for experiment "testExperimentWithAudiences": ["11154"].'
@@ -604,22 +625,23 @@ describe('lib/core/decision_service', function() {
         assert.strictEqual(
           mockLogger.log.args[1][1],
           'DECISION_SERVICE: Audiences for experiment testExperimentWithAudiences collectively evaluated to FALSE.'
-        );
-        assert.strictEqual(
-          mockLogger.log.args[2][1],
-          'DECISION_SERVICE: User testUser does not meet conditions to be in experiment testExperimentWithAudiences.'
         );
       });
 
       it('should return false when audience conditions are not met', function() {
         assert.isFalse(
-          decisionServiceInstance.__checkIfUserIsInAudience(configObj, 'testExperimentWithAudiences', 'testUser', {
-            browser_type: 'chrome',
-          })
+          decisionServiceInstance.__checkIfUserIsInAudience(
+            configObj,
+            'testExperimentWithAudiences',
+            "experiment",
+            'testUser',
+            { browser_type: 'chrome' },
+            ''
+          )
         );
         assert.isTrue(__audienceEvaluateSpy.alwaysReturned(false));
 
-        assert.strictEqual(3, mockLogger.log.callCount);
+        assert.strictEqual(2, mockLogger.log.callCount);
         assert.strictEqual(
           mockLogger.log.args[0][1],
           'DECISION_SERVICE: Evaluating audiences for experiment "testExperimentWithAudiences": ["11154"].'
@@ -627,10 +649,6 @@ describe('lib/core/decision_service', function() {
         assert.strictEqual(
           mockLogger.log.args[1][1],
           'DECISION_SERVICE: Audiences for experiment testExperimentWithAudiences collectively evaluated to FALSE.'
-        );
-        assert.strictEqual(
-          mockLogger.log.args[2][1],
-          'DECISION_SERVICE: User testUser does not meet conditions to be in experiment testExperimentWithAudiences.'
         );
       });
     });
@@ -1239,7 +1257,7 @@ describe('lib/core/decision_service', function() {
                       {
                         id: '1547854156498475',
                         value: '{ "num_buttons": 2, "text": "second variation"}',
-                      },                      
+                      },
                     ],
                     featureEnabled: true,
                     key: 'control',
@@ -1330,11 +1348,6 @@ describe('lib/core/decision_service', function() {
               decisionSource: DECISION_SOURCES.FEATURE_TEST,
             };
             assert.deepEqual(decision, expectedDecision);
-            sinon.assert.calledWithExactly(
-              mockLogger.log,
-              LOG_LEVEL.DEBUG,
-              'DECISION_SERVICE: User user1 is in variation variation of experiment testing_my_feature on the feature test_feature_for_experiment.'
-            );
             sinon.assert.calledWithExactly(getVariationStub, configObj, 'testing_my_feature', 'user1', {
               test_attribute: 'test_value',
             });
@@ -1359,7 +1372,7 @@ describe('lib/core/decision_service', function() {
             sinon.assert.calledWithExactly(
               mockLogger.log,
               LOG_LEVEL.DEBUG,
-              'DECISION_SERVICE: User user1 is not in any experiment on the feature test_feature_for_experiment.'
+              'DECISION_SERVICE: User user1 is not in rollout of feature test_feature_for_experiment.'
             );
           });
         });
@@ -1422,7 +1435,7 @@ describe('lib/core/decision_service', function() {
             sinon.assert.calledWithExactly(
               mockLogger.log,
               LOG_LEVEL.DEBUG,
-              'DECISION_SERVICE: User user1 is in variation var of experiment exp_with_group on the feature feature_with_group.'
+              'BUCKETER: Assigned bucket 593 to user with bucketing ID user1.'
             );
           });
         });
@@ -1445,7 +1458,7 @@ describe('lib/core/decision_service', function() {
             sinon.assert.calledWithExactly(
               mockLogger.log,
               LOG_LEVEL.DEBUG,
-              'DECISION_SERVICE: User user1 is not in any experiment on the feature feature_with_group.'
+              'DECISION_SERVICE: User user1 is not in rollout of feature feature_with_group.'
             );
           });
 
@@ -1461,7 +1474,7 @@ describe('lib/core/decision_service', function() {
             sinon.assert.calledWithExactly(
               mockLogger.log,
               LOG_LEVEL.DEBUG,
-              'DECISION_SERVICE: User user1 is not in any experiment on the feature feature_exp_no_traffic.'
+              'DECISION_SERVICE: There is no rollout of feature feature_exp_no_traffic.'
             );
           });
         });
@@ -1484,7 +1497,7 @@ describe('lib/core/decision_service', function() {
             sinon.assert.calledWithExactly(
               mockLogger.log,
               LOG_LEVEL.DEBUG,
-              'DECISION_SERVICE: User user1 is not in any experiment on the feature feature_with_group.'
+              'DECISION_SERVICE: There is no rollout of feature feature_with_group.'
             );
           });
         });
@@ -1990,11 +2003,6 @@ describe('lib/core/decision_service', function() {
           sinon.assert.calledWithExactly(
             mockLogger.log,
             LOG_LEVEL.DEBUG,
-            'DECISION_SERVICE: User user1 is not in any experiment on the feature shared_feature.'
-          );
-          sinon.assert.calledWithExactly(
-            mockLogger.log,
-            LOG_LEVEL.DEBUG,
             'DECISION_SERVICE: User user1 bucketed into everyone targeting rule.'
           );
           sinon.assert.calledWithExactly(
@@ -2023,11 +2031,6 @@ describe('lib/core/decision_service', function() {
             mockLogger.log,
             LOG_LEVEL.DEBUG,
             'DECISION_SERVICE: Feature unused_flag is not attached to any experiments.'
-          );
-          sinon.assert.calledWithExactly(
-            mockLogger.log,
-            LOG_LEVEL.DEBUG,
-            'DECISION_SERVICE: User user1 is not in any experiment on the feature unused_flag.'
           );
           sinon.assert.calledWithExactly(
             mockLogger.log,

commit aeb389d633e0444310edc4405c73aa3c26632332

fix(bucketer): Don't log invalid variation ID warning when bucketed into a traffic allocation range with empty string entityId (#549)
Summary:

Fix an issue introduced in #515. When bucket returns empty string, we shouldn't log a warning about invalid variation ID. Empty string is a valid value for the entityId property of traffic allocation range objects. There is no invalid variation ID in this situation.

Test plan:

Manually tested with A/B tests and rollouts. Added new unit test.

Issues:

OASIS-6890

packages/optimizely-sdk/lib/core/bucketer/index.js
@@ -114,8 +114,10 @@ export var bucket = function(bucketerParams) {
   var entityId = this._findBucket(bucketValue, bucketerParams.trafficAllocationConfig);
 
   if (!bucketerParams.variationIdMap.hasOwnProperty(entityId)) {
-    var invalidVariationIdLogMessage = sprintf(LOG_MESSAGES.INVALID_VARIATION_ID, MODULE_NAME);
-    bucketerParams.logger.log(LOG_LEVEL.WARNING, invalidVariationIdLogMessage);
+    if (entityId) {
+      var invalidVariationIdLogMessage = sprintf(LOG_MESSAGES.INVALID_VARIATION_ID, MODULE_NAME);
+      bucketerParams.logger.log(LOG_LEVEL.WARNING, invalidVariationIdLogMessage);
+    }
     return null;
   }
 
@@ -128,7 +130,7 @@ export var bucket = function(bucketerParams) {
  * @param {string} bucketingId  Bucketing ID
  * @param {string} userId       ID of user to be bucketed into experiment
  * @param {Object} logger       Logger implementation
- * @return {string} ID of experiment if user is bucketed into experiment within the group, null otherwise
+ * @return {string|null} ID of experiment if user is bucketed into experiment within the group, null otherwise
  */
 export var bucketUserIntoExperiment = function(group, bucketingId, userId, logger) {
   var bucketingKey = sprintf('%s%s', bucketingId, group.id);
@@ -148,7 +150,7 @@ export var bucketUserIntoExperiment = function(group, bucketingId, userId, logge
  * @param  {Object[]} trafficAllocationConfig
  * @param  {number}   trafficAllocationConfig[].endOfRange
  * @param  {number}   trafficAllocationConfig[].entityId
- * @return {string}   Entity ID for bucketing if bucket value is within traffic allocation boundaries, null otherwise
+ * @return {string|null}   Entity ID for bucketing if bucket value is within traffic allocation boundaries, null otherwise
  */
 export var _findBucket = function(bucketValue, trafficAllocationConfig) {
   for (var i = 0; i < trafficAllocationConfig.length; i++) {

packages/optimizely-sdk/lib/core/bucketer/index.tests.js
@@ -286,6 +286,15 @@ describe('lib/core/bucketer', function() {
           bucketerParamsTest1.userId = 'ppid1';
           expect(bucketer.bucket(bucketerParamsTest1)).to.equal(null);
         });
+
+        it('should not log an invalid variation ID warning', function() {
+          bucketer.bucket(bucketerParams)
+          const foundInvalidVariationWarning = createdLogger.log.getCalls().some((call) => {
+            const message = call.args[1];
+            return message.includes('Bucketed into an invalid variation ID')
+          });
+          expect(foundInvalidVariationWarning).to.equal(false);
+        });
       });
 
       describe('when the traffic allocation has invalid variation ids', function() {