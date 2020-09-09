079e6ac1ea19865f5bf1e135e815a660f5a06102
Polina Nguen <polina.nguen@optimizely.com>
2020-09-09 09:16:13 -0700

    Revert audience_evaluator to js

--- packages/optimizely-sdk/lib/shared_types.ts
+++ packages/optimizely-sdk/lib/shared_types.ts
@@ -3,12 +3,3 @@ export type UserAttributes = {
    // eslint-disable-next-line  @typescript-eslint/no-explicit-any
    [name: string]: any;
 }
-
-export type Condition = {
-   name: string;
-   type: string;
-   match?: string;
-   value: string | number | boolean | null;
- }
-
-export type ConditionEvaluator = (condition: Condition, userAttributes: UserAttributes) => boolean | null;
