952735214f3515d0a6c1493d1060932c7fe8827f
uzair-folio3 <unadeem@folio3.com>
2020-09-16 16:37:05 +0500

    Update index.d.ts

---
+++ packages/optimizely-sdk/lib/core/notification_center/index.d.ts
@@ -0,0 +1,44 @@
+/**
+ * Copyright 2020, Optimizely
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+declare module '@optimizely/optimizely-sdk/lib/core/notification_center' {
+    import { LogHandler, ErrorHandler } from '@optimizely/js-sdk-logging';
+
+    interface Options {
+        logger: LogHandler;
+        errorHandle: ErrorHandler;
+    }
+
+    export interface NotificationCenter {
+        _logger: LogHandler;
+        _errorHandler: ErrorHandler;
+        // TODO[OASIS-6649]: Don't use object type
+        // eslint-disable-next-line  @typescript-eslint/ban-types
+        __notificationListeners: Object;
+
+        NotificationCenter(options: Options): void
+        // TODO[OASIS-6649]: Don't use any type
+        // eslint-disable-next-line  @typescript-eslint/no-explicit-any 
+        addNotificationListener(notificationType: string, callback: (...args: any[]) => any): number | boolean
+        removeNotificationListener(listenerId: number): boolean
+        clearAllNotificationListeners(): void
+        clearNotificationListeners(notificationType: string): void
+        // TODO[OASIS-6649]: Don't use object type
+        // eslint-disable-next-line  @typescript-eslint/ban-types
+        sendNotifications(notificationType: string, notificationData: Object): void
+        createNotificationCenter(options: Options): NotificationCenter
+    }
+}
