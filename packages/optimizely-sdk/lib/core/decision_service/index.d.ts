a0805d72d63afe0fc43d5070894c8493696fe092
Polina Nguen <polina.nguen@optimizely.com>
2020-09-17 09:05:53 -0700

    Fix imports

--- packages/optimizely-sdk/lib/core/decision_service/index.d.ts
+++ packages/optimizely-sdk/lib/core/decision_service/index.d.ts
@@ -14,8 +14,11 @@
  * limitations under the License.
  */
 
-declare module '@optimizely/optimizely-sdk/lib/core/decision_service' { 
+declare module '@optimizely/optimizely-sdk/lib/core/decision_service' {
   import { LogHandler } from '@optimizely/js-sdk-logging';
+  import { UserProfile, UserAttributes } from '@optimizely/optimizely-sdk';
+  import { ProjectConfig } from '@optimizely/optimizely-sdk/lib/core/project_config';
+  import { Experiment, Variation, FeatureFlag } from '@optimizely/optimizely-sdk/lib/core/project_config';
 
   export function createDecisionService(options: Options): DecisionService;
 
@@ -26,153 +29,16 @@ declare module '@optimizely/optimizely-sdk/lib/core/decision_service' {
 
   interface DecisionService {
     getVariation(experimentKey: string, userId: string, attributes?: UserAttributes): string | null;
-    getVariationForFeature(configObj: ConfigObj, feature: FeatureFlag, userId: string, attributes: unknown): Decision;
+    getVariationForFeature(configObj: ProjectConfig, feature: FeatureFlag, userId: string, attributes: unknown): Decision;
     removeForcedVariation(userId: unknown, experimentId: string, experimentKey: string): void;
-    getForcedVariation(configObj: ConfigObj, experimentKey: string, userId: string): string | null;
-    setForcedVariation(configObj: ConfigObj, experimentKey: string, userId: string, variationKey: unknown): boolean;
-  }
-
-  // Information about past bucketing decisions for a user.
-  interface UserProfile {
-    user_id: string;
-    experiment_bucket_map: {
-      [experiment_id: string]: {
-        variation_id: string;
-      };
-    };
+    getForcedVariation(configObj: ProjectConfig, experimentKey: string, userId: string): string | null;
+    setForcedVariation(configObj: ProjectConfig, experimentKey: string, userId: string, variationKey: unknown): boolean;
   }
 
   interface Options {
     userProfileService: UserProfileService | null;
     logger: LogHandler;
     UNSTABLE_conditionEvaluators: unknown;
-  }
-
-  interface ConfigObj {
-    events: Event[];
-    featureFlags: FeatureFlag[];
-    experiments: Experiment[];
-    anonymizeIP: boolean;
-    botFiltering: boolean;
-    audiences: Audience[];
-    revision: string;
-    groups: Group[];
-    attributes: UserAttributes;
-    rollouts: Rollout[];
-    projectId: string;
-    accountId: string;
-    version: string;
-    variables: FeatureVariable[];
-    audiencesById: {[key : string]: Audience};
-    attributeKeyMap: attributeKeyMap;
-    groupIdMap: {[key: string]: Group};
-    rolloutIdMap: {[key:string]: Rollout};
-    experimentKeyMap: {[key: string]: Experiment}
-    experimentIdMap: {[key: string]: Experiment};
-    variationIdMap: {[key: string]: Variation};
-    variationVariableUsageMap: {[key: string]: ExperimentVariableByID};
-    experimentFeatureMap: {[key: string]: string[]};
-    featureKeyMap: {[key: string]: FeatureFlag};
-  }
-
-  interface Event {
-    key: string;
-    id: string;
-    experimentIds: string[]
-  }
-
-  interface FeatureFlag {
-    rolloutId: string;
-    key: string;
-    id: string;
-    experimentIds: string[],
-    variables: FeatureVariable[],
-    variableKeyMap?: {[key: string]: FeatureVariable}
-  }
-
-  interface FeatureVariable {
-    type: string;
-    subType?: string;
-    key: string;
-    id: string;
-    defaultValue: string;
-  }
-
-  interface Experiment {
-  id: string;
-  key: string;
-  status: string;
-  layerId: string;
-  variations: Variation[];
-  trafficAllocation: Array<TrafficAllocationEntity>;
-  audienceIds: string[];
-    // TODO[OASIS-6649]: Don't use object type
-    // eslint-disable-next-line  @typescript-eslint/ban-types
-  forcedVariations: object;
-  variationKeyMap?: {[key: string]: Variation};
-  }
-
-  interface Variation {
-    id: string;
-    key: string;
-    featureEnabled: boolean;
-    variables: ExperimentVariable[];
-  }
-  
-  interface Audience {
-    id: string;
-    name: string;
-    conditions: ConditionTree<Condition>;
-  }
-
-  interface Group {
-    policy: string;
-    id: string;
-    experiments: Experiment[];
-    trafficAllocation: Array<TrafficAllocationEntity>;
-  }
-
-  type UserAttributes = {
-    // TODO[OASIS-6649]: Don't use any type
-    // eslint-disable-next-line  @typescript-eslint/no-explicit-any
-    [name: string]: any;
-  }
-
-  type Condition = {
-    name: string;
-    type: string;
-    match?: string;
-    value: string | number | boolean | null;
-  }
-  
-  type ConditionTree<Leaf> = Leaf | unknown[];
-
-  interface TrafficAllocationEntity {
-    endOfRange: number;
-    entityId: string;
-  }
-
-  interface Rollout {
-    id: string;
-    experiments: Experiment[];
-  }
-
-  interface Attribute {
-    id: string;
-    key: string;
-  }
-
-  interface attributeKeyMap {
-    [key: string]: Attribute;
-  }
-
-  type ExperimentVariable = {
-    id: string;
-    value: string;
-  }
-
-  type ExperimentVariableByID = {
-    [key: string]: ExperimentVariable;
   }
 
   interface Decision {
--- packages/optimizely-sdk/lib/index.d.ts
+++ packages/optimizely-sdk/lib/index.d.ts
@@ -16,7 +16,7 @@
 
 declare module '@optimizely/optimizely-sdk' {
   import { LogHandler, ErrorHandler } from '@optimizely/js-sdk-logging';
-  import { UserProfileService } from '@optimizely/optimizely-sdk/lib/core/decision_service';
+  import { Experiment, Variation } from '@optimizely/optimizely-sdk/lib/core/project_config';
   import * as enums from '@optimizely/optimizely-sdk/lib/utils/enums';
   import * as logging from '@optimizely/optimizely-sdk/lib/plugins/logger';
   export { enums, logging };
@@ -136,6 +136,11 @@ declare module '@optimizely/optimizely-sdk' {
     dispatchEvent: (event: Event, callback: () => void) => void;
   }
 
+  export interface UserProfileService {
+    lookup: (userId: string) => UserProfile;
+    save: (profile: UserProfile) => void;
+  }
+
   // NotificationCenter-related types
   export interface NotificationCenter {
     addNotificationListener<T extends ListenerPayload>(
@@ -176,25 +181,14 @@ declare module '@optimizely/optimizely-sdk' {
     logEvent: Event;
   }
 
-  export interface Experiment {
-    id: string;
-    key: string;
-    status: string;
-    layerId: string;
-    variations: Variation[];
-    trafficAllocation: Array<{
-      entityId: string;
-      endOfRange: number;
-    }>;
-    audienceIds: string[];
-    // TODO[OASIS-6649]: Don't use object type
-    // eslint-disable-next-line  @typescript-eslint/ban-types
-    forcedVariations: object;
-  }
-
-  export interface Variation {
-    id: string;
-    key: string;
+  // Information about past bucketing decisions for a user.
+  export interface UserProfile {
+    user_id: string;
+    experiment_bucket_map: {
+      [experiment_id: string]: {
+        variation_id: string;
+      };
+    };
   }
 
   /**
