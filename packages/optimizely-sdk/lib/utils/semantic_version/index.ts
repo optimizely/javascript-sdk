f49f64b6de012d92282fbad95fcca0c512819a22
uzair-folio3 <unadeem@folio3.com>
2020-09-19 00:07:54 +0500

    Update index.ts

--- packages/optimizely-sdk/lib/utils/semantic_version/index.ts
+++ packages/optimizely-sdk/lib/utils/semantic_version/index.ts
@@ -20,77 +20,77 @@ const MODULE_NAME = 'SEMANTIC VERSION';
 const logger = getLogger();
 
 /**
-  * Evaluate if provided string is number only
-  * @param  {unknown}  content
-  * @return {boolean}  true if the string is number only
-  *                     
-  */
+ * Evaluate if provided string is number only
+ * @param  {unknown}  content
+ * @return {boolean}  true if the string is number only
+ *
+ */
 function isNumber(content: string): boolean {
   return /^\d+$/.test(content);
 }
 
 /**
-* Evaluate if provided version contains pre-release "-"
-* @param  {unknown}  version
-* @return {boolean}  true if the version contains "-" and meets condition
-*                     
-*/
+ * Evaluate if provided version contains pre-release "-"
+ * @param  {unknown}  version
+ * @return {boolean}  true if the version contains "-" and meets condition
+ *
+ */
 function isPreReleaseVersion(version: string): boolean {
   const preReleaseIndex = version.indexOf(VERSION_TYPE.PRE_RELEASE_VERSION_DELIMITER);
   const buildIndex = version.indexOf(VERSION_TYPE.BUILD_VERSION_DELIMITER);
 
   if (preReleaseIndex < 0) {
-      return false;
+    return false;
   }
 
-  if (buildIndex < 0 ) {
-      return true;
+  if (buildIndex < 0) {
+    return true;
   }
 
   return preReleaseIndex < buildIndex;
 }
-  
+
 /**
  * Evaluate if provided version contains build "+"
  * @param  {unknown}  version
  * @return {boolean}  true if the version contains "+" and meets condition
- *                     
+ *
  */
 function isBuildVersion(version: string): boolean {
   const preReleaseIndex = version.indexOf(VERSION_TYPE.PRE_RELEASE_VERSION_DELIMITER);
   const buildIndex = version.indexOf(VERSION_TYPE.BUILD_VERSION_DELIMITER);
 
   if (buildIndex < 0) {
-      return false;
+    return false;
   }
 
-  if (preReleaseIndex < 0 ) {
-      return true;
+  if (preReleaseIndex < 0) {
+    return true;
   }
 
   return buildIndex < preReleaseIndex;
 }
-  
+
 /**
  * check if there is any white spaces " " in version
  * @param  {unknown}  version
  * @return {boolean}  true if the version contains " "
- *                     
+ *
  */
 function hasWhiteSpaces(version: string): boolean {
   return /\s/.test(version);
 }
-  
+
 /**
  * split version in parts
  * @param  {unknown}  version
  * @return {boolean}  The array of version split into smaller parts i.e major, minor, patch etc
- *                    null if given version is in invalid format 
+ *                    null if given version is in invalid format
  */
 function splitVersion(version: string): string[] | null {
   let targetPrefix = version;
   let targetSuffix = '';
-  
+
   // check that version shouldn't have white space
   if (hasWhiteSpaces(version)) {
     logger.warn(LOG_MESSAGES.UNKNOWN_MATCH_TYPE, MODULE_NAME, version);
@@ -101,43 +101,41 @@ function splitVersion(version: string): string[] | null {
   if (isPreReleaseVersion(version)) {
     targetPrefix = version.substring(0, version.indexOf(VERSION_TYPE.PRE_RELEASE_VERSION_DELIMITER));
     targetSuffix = version.substring(version.indexOf(VERSION_TYPE.PRE_RELEASE_VERSION_DELIMITER) + 1);
-  }
-  else if (isBuildVersion(version)) {
-      targetPrefix = version.substring(0, version.indexOf(VERSION_TYPE.BUILD_VERSION_DELIMITER));
-      targetSuffix = version.substring(version.indexOf(VERSION_TYPE.BUILD_VERSION_DELIMITER) + 1);
+  } else if (isBuildVersion(version)) {
+    targetPrefix = version.substring(0, version.indexOf(VERSION_TYPE.BUILD_VERSION_DELIMITER));
+    targetSuffix = version.substring(version.indexOf(VERSION_TYPE.BUILD_VERSION_DELIMITER) + 1);
   }
 
   // check dot counts in target_prefix
   if (typeof targetPrefix !== 'string' || typeof targetSuffix !== 'string') {
-      return null;
+    return null;
   }
 
-  const dotCount = targetPrefix.split(".").length - 1;
-  if (dotCount > 2){
-  logger.warn(LOG_MESSAGES.UNKNOWN_MATCH_TYPE, MODULE_NAME, version);
-  return null;
+  const dotCount = targetPrefix.split('.').length - 1;
+  if (dotCount > 2) {
+    logger.warn(LOG_MESSAGES.UNKNOWN_MATCH_TYPE, MODULE_NAME, version);
+    return null;
   }
 
-  const targetVersionParts = targetPrefix.split(".")
+  const targetVersionParts = targetPrefix.split('.');
   if (targetVersionParts.length != dotCount + 1) {
-  logger.warn(LOG_MESSAGES.UNKNOWN_MATCH_TYPE, MODULE_NAME, version);
-  return null;
+    logger.warn(LOG_MESSAGES.UNKNOWN_MATCH_TYPE, MODULE_NAME, version);
+    return null;
   }
-  for (const part of targetVersionParts){
-  if (!isNumber(part)) {
+  for (const part of targetVersionParts) {
+    if (!isNumber(part)) {
       logger.warn(LOG_MESSAGES.UNKNOWN_MATCH_TYPE, MODULE_NAME, version);
       return null;
-  }
-  }
-  
-  if (targetSuffix) {
-      targetVersionParts.push(targetSuffix)
+    }
   }
 
-  return targetVersionParts
-    
+  if (targetSuffix) {
+    targetVersionParts.push(targetSuffix);
+  }
+
+  return targetVersionParts;
 }
-  
+
 /**
  * Compare user version with condition version
  * @param  {string}  conditionsVersion
@@ -159,24 +157,20 @@ export function compareVersion(conditionsVersion: string, userProvidedVersion: s
 
   for (let idx = 0; idx < conditionsVersionParts.length; idx++) {
     if (userVersionPartsLen <= idx) {
-        return isPreReleaseVersion(conditionsVersion) || isBuildVersion(conditionsVersion) ? 1 : -1;
-    }
-    else if (!isNumber(userVersionParts[idx])) {
+      return isPreReleaseVersion(conditionsVersion) || isBuildVersion(conditionsVersion) ? 1 : -1;
+    } else if (!isNumber(userVersionParts[idx])) {
       if (userVersionParts[idx] < conditionsVersionParts[idx]) {
         return isPreReleaseVersion(conditionsVersion) && !isPreReleaseVersion(userProvidedVersion) ? 1 : -1;
-      }
-      else if (userVersionParts[idx] > conditionsVersionParts[idx]) {
+      } else if (userVersionParts[idx] > conditionsVersionParts[idx]) {
         return !isPreReleaseVersion(conditionsVersion) && isPreReleaseVersion(userProvidedVersion) ? -1 : 1;
       }
-    }
-    else {
+    } else {
       const userVersionPart = parseInt(userVersionParts[idx]);
       const conditionsVersionPart = parseInt(conditionsVersionParts[idx]);
       if (userVersionPart > conditionsVersionPart) {
-          return 1;
-      }
-      else if (userVersionPart < conditionsVersionPart) {
-          return -1;
+        return 1;
+      } else if (userVersionPart < conditionsVersionPart) {
+        return -1;
       }
     }
   }
@@ -185,7 +179,6 @@ export function compareVersion(conditionsVersion: string, userProvidedVersion: s
   if (isPreReleaseVersion(userProvidedVersion) && !isPreReleaseVersion(conditionsVersion)) {
     return -1;
   }
-    
+
   return 0;
 }
-  
