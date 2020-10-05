c0648394c559964cb2b089950c7f61a2344f769b
uzair-folio3 <unadeem@folio3.com>
2020-10-05 18:07:56 +0500

    Update roolup config for ts file

--- packages/optimizely-sdk/rollup.config.js
+++ packages/optimizely-sdk/rollup.config.js
@@ -43,7 +43,7 @@ const cjsBundleFor = (platform) => ({
     typescript(typescriptPluginOptions),
   ],
   external: ['https', 'http', 'url'].concat(Object.keys(dependencies || {})),
-  input: `lib/index.${platform}.js`,
+  input: `lib/index.${platform}.ts`,
   output: {
     exports: 'named',
     format: 'cjs',
