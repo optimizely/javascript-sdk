b7bacfc8c04a2d6eac8b001749bb1438e36fbd8a
Polina Nguen <polina.nguen@optimizely.com>
2020-10-30 12:21:47 -0700

    Prepare for 4.4.0 release

--- packages/optimizely-sdk/lib/index.node.tests.js
+++ packages/optimizely-sdk/lib/index.node.tests.js
@@ -90,7 +90,7 @@ describe('optimizelyFactory', function() {
         optlyInstance.onReady().catch(function() {});
 
         assert.instanceOf(optlyInstance, Optimizely);
-        assert.equal(optlyInstance.clientVersion, '4.3.4');
+        assert.equal(optlyInstance.clientVersion, '4.4.0');
       });
 
       describe('event processor configuration', function() {
