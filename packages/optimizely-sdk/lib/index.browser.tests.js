1997ca4a6c221d181cc15f69385bde77d9a68ab2
Polina Nguen <43302774+yavorona@users.noreply.github.com>
2020-11-02 09:21:45 -0800

    chore: Prepare for 4.4.0 release (#607)
    * Prepare for 4.4.0 release
    
    * Update release date

--- packages/optimizely-sdk/lib/index.browser.tests.js
+++ packages/optimizely-sdk/lib/index.browser.tests.js
@@ -148,7 +148,7 @@ describe('javascript-sdk', function() {
         optlyInstance.onReady().catch(function() {});
 
         assert.instanceOf(optlyInstance, Optimizely);
-        assert.equal(optlyInstance.clientVersion, '4.3.4');
+        assert.equal(optlyInstance.clientVersion, '4.4.0');
       });
 
       it('should set the JavaScript client engine and version', function() {
