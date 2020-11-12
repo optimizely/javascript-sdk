91a2b5cefd05932699cb54587ee8e57f94d1cc0b
Polina Nguen <polina.nguen@optimizely.com>
2020-11-12 14:52:16 -0800

    Correct Copyright year

--- packages/optimizely-sdk/lib/core/notification_center/index.ts
+++ packages/optimizely-sdk/lib/core/notification_center/index.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright 2017, 2019-2020, Optimizely
+ * Copyright 2020, Optimizely
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -69,7 +69,7 @@ export class NotificationCenter {
     this.errorHandler = options.errorHandler;
     this.notificationListeners = {};
     objectValues(NOTIFICATION_TYPES).forEach(
-      (notificationTypeEnum: notificationTypesEnum) => {
+      (notificationTypeEnum) => {
         this.notificationListeners[notificationTypeEnum] = [];
       }
     );
