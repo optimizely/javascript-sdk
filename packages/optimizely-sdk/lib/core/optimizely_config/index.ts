e4d8c6ec517a5eace92e0a8fb2f78cd7eba051f3
Polina Nguen <polina.nguen@optimizely.com>
2020-11-25 11:16:57 -0800

    Clean up

--- packages/optimizely-sdk/lib/core/optimizely_config/index.ts
+++ packages/optimizely-sdk/lib/core/optimizely_config/index.ts
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { isFeatureExperiment } from '../project_config';
+import { isFeatureExperiment, ProjectConfig } from '../project_config';
 import {
   OptimizelyExperimentsMap,
   OptimizelyFeaturesMap,
@@ -23,8 +23,6 @@ import {
   Variation,
   Rollout,
 } from '../../shared_types';
-
-import { ProjectConfig } from '../project_config';
 
 interface FeatureVariablesMap {
   [key: string]: FeatureVariable[];
