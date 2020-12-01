c71b9f2739934db89075c8ed60a6816678672b53
Polina Nguen <polina.nguen@optimizely.com>
2020-11-30 16:28:27 -0800

    Add more setAttribute tests

--- packages/optimizely-sdk/lib/optimizely_user_context/index.tests.js
+++ packages/optimizely-sdk/lib/optimizely_user_context/index.tests.js
@@ -24,6 +24,7 @@ import Optimizely from '../optimizely';
 import logger from '../plugins/logger';
 import eventDispatcher from '../plugins/event_dispatcher/index.node';
 import errorHandler from '../plugins/error_handler';
+import * as jsonSchemaValidator from '../utils/json_schema_validator';
 import { getTestProjectConfig } from '../tests/test_data';
 import {
    LOG_LEVEL,
@@ -35,16 +36,17 @@ var testData = getTestProjectConfig();
 
 describe('lib/optimizely_user_context', function() {
   describe('APIs', function() {
-    var optlyInstance;
+    var optimizely;
     var createdLogger = logger.createLogger({
       logLevel: LOG_LEVEL.DEBUG,
     });
     beforeEach(function() {
-      optlyInstance = new Optimizely({
+      optimizely = new Optimizely({
         clientEngine: 'node-sdk',
         datafile: cloneDeep(testData),
         errorHandler: errorHandler,
         eventDispatcher: eventDispatcher,
+        jsonSchemaValidator: jsonSchemaValidator,
         logger: createdLogger,
         isValidInstance: true,
       });
@@ -59,15 +61,19 @@ describe('lib/optimizely_user_context', function() {
     });
 
     describe('#setAttribute', function() {
-      it('should set provided user attributes', function() {
+      it('should set attributes when provided at instantiation of OptimizelyUserContext', function() {
         var userId = 'user1';
         var attributes = { test_attribute: 'test_value' };
-        var user = optlyInstance.createUserContext(userId, attributes);
+        var user = new OptimizelyUserContext({
+          optimizely,
+          userId,
+          attributes
+        });
         user.setAttribute('k1', {'hello': 'there'});
         user.setAttribute('k2', true);
         user.setAttribute('k3', 100);
         user.setAttribute('k4', 3.5);
-        assert.deepEqual(user.getOptimizely(), optlyInstance);
+        assert.deepEqual(user.getOptimizely(), optimizely);
         assert.deepEqual(user.getUserId(), userId);
 
         var newAttributes = user.getAttributes();
@@ -77,6 +83,63 @@ describe('lib/optimizely_user_context', function() {
         assert.deepEqual(newAttributes['k3'], 100);
         assert.deepEqual(newAttributes['k4'], 3.5);
       });
+
+      it('should set attributes when none provided at instantiation of OptimizelyUserContext', function() {
+        var userId = 'user2';
+        var user = new OptimizelyUserContext({
+          optimizely,
+          userId,
+        });
+        user.setAttribute('k1', {'hello': 'there'});
+        user.setAttribute('k2', true);
+        user.setAttribute('k3', 100);
+        user.setAttribute('k4', 3.5);
+        assert.deepEqual(user.getOptimizely(), optimizely);
+        assert.deepEqual(user.getUserId(), userId);
+
+        var newAttributes = user.getAttributes();
+        assert.deepEqual(newAttributes['k1'], {'hello': 'there'});
+        assert.deepEqual(newAttributes['k2'], true);
+        assert.deepEqual(newAttributes['k3'], 100);
+        assert.deepEqual(newAttributes['k4'], 3.5);
+      });
+
+      it('should override existing attributes', function() {
+        var userId = 'user3';
+        var attributes = { test_attribute: 'test_value' };
+        var user = new OptimizelyUserContext({
+          optimizely,
+          userId,
+          attributes,
+        });
+        user.setAttribute('k1', {'hello': 'there'});
+        user.setAttribute('test_attribute', 'overwritten_value');
+        assert.deepEqual(user.getOptimizely(), optimizely);
+        assert.deepEqual(user.getUserId(), userId);
+
+        var newAttributes = user.getAttributes();
+        assert.deepEqual(newAttributes['k1'], {'hello': 'there'});
+        assert.deepEqual(newAttributes['test_attribute'], 'overwritten_value');
+        assert.deepEqual(Object.keys(newAttributes).length, 2);
+      });
+
+      it('should allow to set attributes with value of null', function() {
+        var userId = 'user4';
+        var user = new OptimizelyUserContext({
+          optimizely,
+          userId,
+        });
+        user.setAttribute('null_attribute', null);
+        assert.deepEqual(user.getOptimizely(), optimizely);
+        assert.deepEqual(user.getUserId(), userId);
+
+        var newAttributes = user.getAttributes();
+        assert.deepEqual(newAttributes['null_attribute'], null);
+      });
+    });
+
+    describe('#decide', function() {
+
     });
   });
 });
