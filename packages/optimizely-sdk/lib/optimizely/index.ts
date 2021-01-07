074cdf2fba3807e92b2726faf19570eb4fd44569
Polina Nguen <polina.nguen@optimizely.com>
2021-01-06 20:10:58 -0800

    Update Copyright year to include 2021

--- packages/optimizely-sdk/lib/optimizely/index.ts
+++ packages/optimizely-sdk/lib/optimizely/index.ts
@@ -1,5 +1,5 @@
 /****************************************************************************
- * Copyright 2020, Optimizely, Inc. and contributors                   *
+ * Copyright 2020-2021, Optimizely, Inc. and contributors                   *
  *                                                                          *
  * Licensed under the Apache License, Version 2.0 (the "License");          *
  * you may not use this file except in compliance with the License.         *
