1b0c036593e5f3e9df34373256c80814f1d961d3
Polina Nguen <43302774+yavorona@users.noreply.github.com>
2021-07-15 13:57:31 -0700

    Prepare for 4.6.2 release (#691)

--- packages/optimizely-sdk/lib/index.react_native.tests.js
+++ packages/optimizely-sdk/lib/index.react_native.tests.js
@@ -89,7 +89,7 @@ describe('javascript-sdk/react-native', function() {
         optlyInstance.onReady().catch(function() {});
 
         assert.instanceOf(optlyInstance, Optimizely);
-        assert.equal(optlyInstance.clientVersion, '4.6.1');
+        assert.equal(optlyInstance.clientVersion, '4.6.2');
       });
 
       it('should set the Javascript client engine and version', function() {
