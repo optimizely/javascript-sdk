3bda568a31dae09b1e8215b0fcfefc3f18153ecf
Zeeshan Ashraf <zashraf@folio3.com>
2021-10-06 21:57:15 -0700

    readded logging changes

--- packages/logging/src/logger.ts
+++ packages/logging/src/logger.ts
@@ -218,10 +218,10 @@ class OptimizelyLogger implements LoggerFacade {
    * @param {string} [message]
    * @memberof OptimizelyLogger
    */
-  log(level: LogLevel | string, message: string): void {
+  log(level: LogLevel | string, message: string, ...splat: any[]): void {
     this.internalLog(coerceLogLevel(level), {
       message,
-      splat: [],
+      splat,
     })
   }
 
