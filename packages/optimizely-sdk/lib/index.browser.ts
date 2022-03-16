c3f628eaf730098e8307dbabc8b38c5e0892d1de
ozayr-zaviar <uzairzaviar@gmail.com>
2022-03-16 16:14:55 +0500

    typing synced with index.d.ts

--- packages/optimizely-sdk/lib/index.browser.ts
+++ packages/optimizely-sdk/lib/index.browser.ts
@@ -182,3 +182,32 @@ export default {
   __internalResetRetryState,
   OptimizelyDecideOption,
 };
+
+export {
+  UserAttributes,
+  OptimizelyConfig,
+  OptimizelyVariable,
+  OptimizelyVariation,
+  OptimizelyExperiment,
+  OptimizelyFeature,
+  OptimizelyDecisionContext,
+  OptimizelyForcedDecision,
+  EventTags,
+  Event,
+  EventDispatcher,
+  DatafileOptions,
+  SDKOptions,
+  OptimizelyOptions,
+  UserProfileService,
+  UserProfile,
+  ListenerPayload,
+  OptimizelyDecision,
+  OptimizelyUserContext,
+  NotificationListener,
+  Config,
+  Client,
+  ActivateListenerPayload,
+  TrackListenerPayload,
+} from './shared_types'
+
+export type {NotificationCenter} from "./core/notification_center"
--- packages/optimizely-sdk/lib/index.lite.ts
+++ packages/optimizely-sdk/lib/index.lite.ts
@@ -111,3 +111,32 @@ export default {
   createInstance,
   OptimizelyDecideOption,
 };
+
+export {
+  UserAttributes,
+  OptimizelyConfig,
+  OptimizelyVariable,
+  OptimizelyVariation,
+  OptimizelyExperiment,
+  OptimizelyFeature,
+  OptimizelyDecisionContext,
+  OptimizelyForcedDecision,
+  EventTags,
+  Event,
+  EventDispatcher,
+  DatafileOptions,
+  SDKOptions,
+  OptimizelyOptions,
+  UserProfileService,
+  UserProfile,
+  ListenerPayload,
+  OptimizelyDecision,
+  OptimizelyUserContext,
+  NotificationListener,
+  Config,
+  Client,
+  ActivateListenerPayload,
+  TrackListenerPayload,
+} from './shared_types'
+
+export {NotificationCenter} from "./core/notification_center"
