96ba3b55a5cc77eb27a20e37ead349ac44251cf4
shaharyarsheikh <shaharyarsheikh@folio3.com>
2022-06-06 12:22:18 +0500

    yml updated to 12+ versions

--- packages/optimizely-sdk/lib/core/bucketer/index.tests.js
+++ packages/optimizely-sdk/lib/core/bucketer/index.tests.js
@@ -353,19 +353,9 @@ describe('lib/core/bucketer', function () {
       });
 
       it('should return an error if it cannot generate the hash value', function() {
-       const response = assert.throws(function() {
-          bucketer._generateBucketValue(null);
-        } );
-        expect([
-          sprintf(ERROR_MESSAGES.INVALID_BUCKETING_ID, 'BUCKETER', null, "First argument must be a string, Buffer, ArrayBuffer, Array, or array-like object."), // node v8
-          sprintf(ERROR_MESSAGES.INVALID_BUCKETING_ID, 'BUCKETER', null, "The first argument must be one of type string, Buffer, ArrayBuffer, Array, or Array-like Object. Received type null"),  // node v9
-          sprintf(ERROR_MESSAGES.INVALID_BUCKETING_ID, 'BUCKETER', null, "The first argument must be one of type string, Buffer, ArrayBuffer, Array, or Array-like Object. Received type object"),  // node v10
-          sprintf(ERROR_MESSAGES.INVALID_BUCKETING_ID, 'BUCKETER', null, "The first argument must be of type string or an instance of Buffer, ArrayBuffer, or Array or an Array-like Object. Received null"),  // node v12
-          sprintf(ERROR_MESSAGES.INVALID_BUCKETING_ID, 'BUCKETER', null, "Cannot read property 'length' of null")  // node v14
-        ]).contain(response.message);
-      // assert.throws(function() {
-      //   bucketer._generateBucketValue(null);
-      // }, sprintf(ERROR_MESSAGES.INVALID_BUCKETING_ID, 'BUCKETER', null, "Cannot read property 'length' of null"));
+      assert.throws(function() {
+        bucketer._generateBucketValue(null);
+      }, sprintf(ERROR_MESSAGES.INVALID_BUCKETING_ID, 'BUCKETER', null, "Cannot read property 'length' of null"));
     
       });
     });
