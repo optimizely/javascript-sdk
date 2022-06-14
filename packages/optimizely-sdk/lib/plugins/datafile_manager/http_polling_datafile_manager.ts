fdaf105a40ff3ce00a3bcba654cc80ea567d2979
shaharyarsheikh <shaharyarsheikh@folio3.com>
2022-06-14 11:51:44 +0500

    removed spaces

--- packages/optimizely-sdk/lib/plugins/datafile_manager/http_polling_datafile_manager.ts
+++ packages/optimizely-sdk/lib/plugins/datafile_manager/http_polling_datafile_manager.ts
@@ -1,20 +1,21 @@
 /**
- * Copyright 2021-2022, Optimizely
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+* Copyright 2021-2022, Optimizely
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 import { LoggerFacade } from '@optimizely/js-sdk-logging';
 import { HttpPollingDatafileManager } from '@optimizely/js-sdk-datafile-manager';
+
 import { DatafileOptions, DatafileManagerConfig, DatafileManager } from '../../shared_types';
 import { toDatafile, tryCreatingProjectConfig } from '../../core/project_config';
 import fns from '../../utils/fns';
@@ -25,8 +26,8 @@ export function createHttpPollingDatafileManager(
   // TODO[OASIS-6649]: Don't use object type
   // eslint-disable-next-line  @typescript-eslint/ban-types
   datafile?: string | object,
-  datafileOptions?: DatafileOptions,
-): DatafileManager {  
+  datafileOptions?: DatafileOptions
+): DatafileManager {
   const datafileManagerConfig: DatafileManagerConfig = { sdkKey };
   if (datafileOptions === undefined || (typeof datafileOptions === 'object' && datafileOptions !== null)) {
     fns.assign(datafileManagerConfig, datafileOptions);
@@ -37,7 +38,7 @@ export function createHttpPollingDatafileManager(
       jsonSchemaValidator: undefined,
       logger: logger,
     });
-    
+
     if (error) {
       logger.error(error);
     }
@@ -47,4 +48,3 @@ export function createHttpPollingDatafileManager(
   }
   return new HttpPollingDatafileManager(datafileManagerConfig);
 }
- 
