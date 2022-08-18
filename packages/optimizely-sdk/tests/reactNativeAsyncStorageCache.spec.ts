da42162c27d402d5404d847fef4f4039f3f5f384
Mike Chu <104384559+mikechu-optimizely@users.noreply.github.com>
2022-08-18 14:07:13 -0400

    feat: Add ODP/ATS VuidManager (#776)
    * Add VuidManager tests and empty class file
    
    * Move tests location, Typescript, & jest
    
    * Initial implementation VuidManager (1 test WIP)
    
    * Add persistence cache
    
    * Implement cache into VuidManager
    
    * Correct persistentKeyValueCache's types
    
    * Update persistent cache func descriptions
    
    * Adjust VuidManager tests
    
    * Add React Native and browser persistence classes
    
    * Add React Native and browser persistence classes
    
    * Testing for ReactNativeAsyncStorageCache
    
    * More testing for ReactNativeAsyncStorageCache
    
    * Update to a getter instance()
    
    * BrowserAsyncStorageCache tests
    
    * Bug fix VuidManager and tests
    
    * Add @react-native-async-storage/async-storage
    
    * Update packages/optimizely-sdk/lib/plugins/vuid_manager/index.ts
    
    Co-authored-by: Jae Kim <45045038+jaeopt@users.noreply.github.com>
    
    * Add test around and correct load()
    
    * Delete inMemoryAsyncStorageCache
    
    * Add dev packages react-native-async-storage & jest-ts-auto-mock
    
    * Refactor to use async/await + simplifying returns
    
    * Change to ts-mockito for stubs/mocks
    
    * Add _reset() for testing
    
    * Refactor VUID Manager tests using ts-mockito
    
    * Un-nest vuid stored in cache
    
    * Add JSDoc
    
    * Corrections to accessibility and interface
    
    * Corrections to tests
    
    * Update to package-lock
    
    * Fix to AsyncStorageCache types and tests
    
    Co-authored-by: Jae Kim <45045038+jaeopt@users.noreply.github.com>

---
+++ packages/optimizely-sdk/tests/reactNativeAsyncStorageCache.spec.ts
@@ -0,0 +1,90 @@
+/**
+ * Copyright 2020, 2022, Optimizely
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/// <reference types="jest" />
+
+import ReactNativeAsyncStorageCache from '../lib/plugins/key_value_cache/reactNativeAsyncStorageCache';
+import AsyncStorage from '../__mocks__/@react-native-async-storage/async-storage';
+
+describe('ReactNativeAsyncStorageCache', () => {
+  const TEST_OBJECT_KEY = 'testObject';
+  const testObject = { name: 'An object', with: { some: 2, properties: ['one', 'two'] } };
+  let cacheInstance: ReactNativeAsyncStorageCache;
+
+  beforeAll(() => {
+    cacheInstance = new ReactNativeAsyncStorageCache();
+  });
+
+  beforeEach(() => {
+    AsyncStorage.clearStore();
+    AsyncStorage.setItem(TEST_OBJECT_KEY, JSON.stringify(testObject));
+  });
+
+  describe('contains', () => {
+    it('should return true if value with key exists', async () => {
+      const keyWasFound = await cacheInstance.contains(TEST_OBJECT_KEY);
+
+      expect(keyWasFound).toBe(true);
+    });
+
+    it('should return false if value with key does not exist', async () => {
+      const keyWasFound = await cacheInstance.contains('keyThatDoesNotExist');
+
+      expect(keyWasFound).toBe(false);
+    });
+  });
+
+  describe('get', () => {
+    it('should return correct string when item is found in cache', async () => {
+      const json = await cacheInstance.get(TEST_OBJECT_KEY);
+      const parsedObject = JSON.parse(json ?? '');
+
+      expect(parsedObject).toEqual(testObject);
+    });
+
+    it('should return empty string if item is not found in cache', async () => {
+      const json = await cacheInstance.get('keyThatDoesNotExist');
+
+      expect(json).toBeNull();
+    });
+  });
+
+  describe('remove', () => {
+    it('should return true after removing a found entry', async () => {
+      const wasSuccessful = await cacheInstance.remove(TEST_OBJECT_KEY);
+
+      expect(wasSuccessful).toBe(true);
+    });
+
+    it('should return false after trying to remove an entry that is not found ', async () => {
+      const wasSuccessful = await cacheInstance.remove('keyThatDoesNotExist');
+
+      expect(wasSuccessful).toBe(false);
+    });
+  });
+
+  describe('set', () => {
+    it('should resolve promise if item was successfully set in the cache', async () => {
+      const anotherTestStringValue = 'This should be found too.';
+
+      await cacheInstance.set('anotherTestStringValue', anotherTestStringValue);
+
+      const itemsInReactAsyncStorage = AsyncStorage.dumpItems();
+      expect(itemsInReactAsyncStorage['anotherTestStringValue']).toEqual(anotherTestStringValue);
+      expect(itemsInReactAsyncStorage[TEST_OBJECT_KEY]).toEqual(JSON.stringify(testObject));
+    });
+  });
+});
