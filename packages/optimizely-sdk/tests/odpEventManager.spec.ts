010370ea1ae66617073165360b773d624ab89b27
Mike Chu <michael.chu@optimizely.com>
2022-09-21 13:56:26 -0400

    ODP Event Manager test layout

---
+++ packages/optimizely-sdk/tests/odpEventManager.spec.ts
@@ -0,0 +1,59 @@
+/**
+ * Copyright 2022, Optimizely
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/// <reference types="jest" />
+
+describe('OdpEventManager', () => {
+  it('should log and discard events when event manager not running', () => {
+
+  });
+
+  it('should log and discard events when event manager is not ready', () => {
+
+  });
+
+  it('should log a max queue hit and discard ', () => {
+
+  });
+
+  it('should dispatch events in correct number of batches', () => {
+
+  });
+
+  it('should dispatch events with correct payload', () => {
+
+  });
+
+  it('should dispatch events with correct flush interval', () => {
+
+  });
+
+  it('should retry failed events', () => {
+
+  });
+
+  it('should flush all scheduled events before stopping', () => {
+
+  });
+
+  it('should prepare correct payload for identify user', () => {
+
+  });
+
+  it('should apply updated ODP configuration when available', () => {
+
+  });
+});
