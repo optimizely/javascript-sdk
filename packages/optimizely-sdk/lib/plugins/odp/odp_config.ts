7e0a88195d9fb826dcfc08515aadec48ff54d862
Mike Chu <michael.chu@optimizely.com>
2022-10-03 17:27:01 -0400

    Review code changes

--- packages/optimizely-sdk/lib/plugins/odp/odp_config.ts
+++ packages/optimizely-sdk/lib/plugins/odp/odp_config.ts
@@ -68,8 +68,18 @@ export class OdpConfig {
   constructor(apiKey: string, apiHost: string, segmentsToCheck?: string[]) {
     this._apiKey = apiKey;
     this._apiHost = apiHost;
-    this._segmentsToCheck = segmentsToCheck ?? [] as string[];
-    this._odpServiceIntegrated = this._apiKey && this._apiHost ? ODP_CONFIG_STATE.INTEGRATED : ODP_CONFIG_STATE.UNDETERMINED;
+    this._segmentsToCheck = segmentsToCheck ?? [];
+    this._odpServiceIntegrated = OdpConfig.determineOdpServiceIntegration(this._apiKey, this.apiHost);
+  }
+
+  /**
+   * Determine the value of Service Integration enum
+   * @param apiKey ODP API Key
+   * @param apiHost Server host of the API
+   * @private
+   */
+  private static determineOdpServiceIntegration(apiKey: string, apiHost: string): ODP_CONFIG_STATE {
+    return apiKey && apiHost ? ODP_CONFIG_STATE.INTEGRATED : ODP_CONFIG_STATE.NOT_INTEGRATED;
   }
 
   /**
@@ -80,7 +90,7 @@ export class OdpConfig {
    * @returns true if configuration was updated successfully
    */
   public update(apiKey: string, apiHost: string, segmentsToCheck: string[]): boolean {
-    this._odpServiceIntegrated = apiKey && apiHost ? ODP_CONFIG_STATE.INTEGRATED : ODP_CONFIG_STATE.NOT_INTEGRATED;
+    this._odpServiceIntegrated = OdpConfig.determineOdpServiceIntegration(apiKey, apiHost);
 
     if (this._apiKey === apiKey && this._apiHost === apiHost && this._segmentsToCheck === segmentsToCheck) {
       return false;
