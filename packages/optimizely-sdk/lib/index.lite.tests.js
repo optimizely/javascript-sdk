2009d922276bb4e2ec2629df8f2a627b0553adae
Raju Ahmed <raju.ahmed@optimizely.com>
2023-10-11 21:26:08 +0600

    [FSSDK-9605] prepare for release 4.10.0

--- packages/optimizely-sdk/lib/index.lite.tests.js
+++ packages/optimizely-sdk/lib/index.lite.tests.js
@@ -76,7 +76,7 @@ describe('optimizelyFactory', function () {
         optlyInstance.onReady().catch(function () { });
 
         assert.instanceOf(optlyInstance, Optimizely);
-        assert.equal(optlyInstance.clientVersion, '4.9.4');
+        assert.equal(optlyInstance.clientVersion, '4.10.0');
       });
     });
   });
--- packages/optimizely-sdk/lib/index.node.tests.js
+++ packages/optimizely-sdk/lib/index.node.tests.js
@@ -90,7 +90,7 @@ describe('optimizelyFactory', function () {
         optlyInstance.onReady().catch(function () { });
 
         assert.instanceOf(optlyInstance, Optimizely);
-        assert.equal(optlyInstance.clientVersion, '4.9.4');
+        assert.equal(optlyInstance.clientVersion, '4.10.0');
       });
 
       describe('event processor configuration', function () {
