097b54f8a93be6a4d4a0b4d2191e107f34e1955b
Mike Chu <michael.chu@optmizely.com>
2024-02-09 09:24:41 -0500

    docs: update copyright notices

--- lib/index.node.ts
+++ lib/index.node.ts
@@ -1,11 +1,11 @@
 /****************************************************************************
- * Copyright 2016-2017, 2019-2023 Optimizely, Inc. and contributors        *
+ * Copyright 2016-2017, 2019-2024 Optimizely, Inc. and contributors        *
  *                                                                          *
  * Licensed under the Apache License, Version 2.0 (the "License");          *
  * you may not use this file except in compliance with the License.         *
  * You may obtain a copy of the License at                                  *
  *                                                                          *
- *    https://www.apache.org/licenses/LICENSE-2.0                            *
+ *    https://www.apache.org/licenses/LICENSE-2.0                           *
  *                                                                          *
  * Unless required by applicable law or agreed to in writing, software      *
  * distributed under the License is distributed on an "AS IS" BASIS,        *
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and      *
  * limitations under the License.                                           *
  ***************************************************************************/
+
 import { getLogger, setErrorHandler, getErrorHandler, LogLevel, setLogHandler, setLogLevel } from './modules/logging';
 import Optimizely from './optimizely';
 import * as enums from './utils/enums';
