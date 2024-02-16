47d4950ffd2ce0de548682768008d3dc5ca2d56e
Mike Chu <michael.chu@optmizely.com>
2024-02-16 14:49:11 -0500

    ci: bump version to 5.0.1

--- lib/utils/enums/index.ts
+++ lib/utils/enums/index.ts
@@ -223,8 +223,8 @@ export const NODE_CLIENT_ENGINE = 'node-sdk';
 export const REACT_CLIENT_ENGINE = 'react-sdk';
 export const REACT_NATIVE_CLIENT_ENGINE = 'react-native-sdk';
 export const REACT_NATIVE_JS_CLIENT_ENGINE = 'react-native-js-sdk';
-export const BROWSER_CLIENT_VERSION = '5.0.0';
-export const NODE_CLIENT_VERSION = '5.0.0';
+export const BROWSER_CLIENT_VERSION = '5.0.1';
+export const NODE_CLIENT_VERSION = '5.0.1';
 
 export const DECISION_NOTIFICATION_TYPES = {
   AB_TEST: 'ab-test',
