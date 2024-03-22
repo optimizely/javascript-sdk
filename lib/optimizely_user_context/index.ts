a1e6110cc48acd661f433c4072edf48274e13130
Mike Chu <104384559+mikechu-optimizely@users.noreply.github.com>
2024-03-22 05:12:56 -0400

    [FSSDK-9984] fix: Empty fetched qualified segments validity (#916)
    * fix: empty segments array is valid; null segments is invalid
    
    * test: add tests around segment valid returns
    
    ---------
    
    Co-authored-by: Mike Chu <michael.chu@optmizely.com>

--- lib/optimizely_user_context/index.ts
+++ lib/optimizely_user_context/index.ts
@@ -258,7 +258,7 @@ export default class OptimizelyUserContext implements IOptimizelyUserContext {
 
     this.qualifiedSegments = segments;
 
-    return !!segments;
+    return segments !== null;
   }
 
   /**
