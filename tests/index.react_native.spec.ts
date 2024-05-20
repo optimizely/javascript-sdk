43fea02411b583051f8b38e232d474bfba9f283b
pulak-opti <129880418+pulak-opti@users.noreply.github.com>
2024-05-20 20:38:18 +0600

    [FSSDK-10180] prepare for release 5.3.1 (#931)

--- tests/index.react_native.spec.ts
+++ tests/index.react_native.spec.ts
@@ -90,7 +90,7 @@ describe('javascript-sdk/react-native', () => {
 
         expect(optlyInstance).toBeInstanceOf(Optimizely);
         // @ts-ignore
-        expect(optlyInstance.clientVersion).toEqual('5.3.1-rc.1');
+        expect(optlyInstance.clientVersion).toEqual('5.3.1');
       });
 
       it('should set the React Native JS client engine and javascript SDK version', () => {
