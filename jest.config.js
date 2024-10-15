699174388de4a78fa134d03b64cc166432f7045d
Mike Chu <michael.chu@optimizely.com>
2024-10-15 21:47:51 +0000

    chore: another correction to jest config

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   "transform": {
     "^.+\\.(ts|tsx|js|jsx)$": "ts-jest",
   },
-  "testRegex": "(/tests/.*|(\\.|/)(test|spec|tests))\\.tsx?$",
+  "testRegex": "(/tests/.*|(\\.|/)(test|spec|tests))\\.(tsx?|js)$",
   moduleNameMapper: {
     // Force module uuid to resolve with the CJS entry point, because Jest does not support package.json.exports. See https://github.com/uuidjs/uuid/issues/451
     "uuid": require.resolve('uuid'),
