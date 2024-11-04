d20228b379fda85114a86cf5ce05a56ed2c43ba3
Md Junaed Hossain <169046794+junaed-optimizely@users.noreply.github.com>
2024-11-04 22:00:05 +0600

    [FSSDK-10766] UPS batch processing (#953)
    decide & decideForKeys adjustment

--- lib/utils/enums/index.ts
+++ lib/utils/enums/index.ts
@@ -128,7 +128,8 @@ export const LOG_MESSAGES = {
   RETURNING_STORED_VARIATION:
     '%s: Returning previously activated variation "%s" of experiment "%s" for user "%s" from user profile.',
   ROLLOUT_HAS_NO_EXPERIMENTS: '%s: Rollout of feature %s has no experiments',
-  SAVED_VARIATION: '%s: Saved variation "%s" of experiment "%s" for user "%s".',
+  SAVED_USER_VARIATION: '%s: Saved user profile for user "%s".',
+  UPDATED_USER_VARIATION: '%s: Updated variation "%s" of experiment "%s" for user "%s".',
   SAVED_VARIATION_NOT_FOUND:
     '%s: User %s was previously bucketed into variation with ID %s for experiment %s, but no matching variation was found.',
   SHOULD_NOT_DISPATCH_ACTIVATE: '%s: Experiment %s is not in "Running" state. Not activating user.',
