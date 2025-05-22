b6ece7abcb8e44d5e4b1ab641198098291dddd48
Raju Ahmed <raju.ahmed@optimizely.com>
2025-05-22 20:48:15 +0600

    up

--- lib/project_config/project_config.ts
+++ lib/project_config/project_config.ts
@@ -116,8 +116,6 @@ const RESERVED_ATTRIBUTE_PREFIX = '$opt_';
 function createMutationSafeDatafileCopy(datafile: any): ProjectConfig {
   const datafileCopy = { ...datafile };
 
-  console.log(datafile, datafileCopy);
-
   datafileCopy.audiences = (datafile.audiences || []).map((audience: Audience) => {
     return { ...audience };
   });
