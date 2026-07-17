deeda8dc6a0ae913489ab0f5ef7f8d07c433f8f4
Farhan Anjum <Farhan.Anjum@optimizely.com>
2026-07-17 22:42:26 +0600

    test: switch to rc.1 version for OIDC test

--- lib/utils/enums/index.ts
+++ lib/utils/enums/index.ts
@@ -44,7 +44,7 @@ export const CONTROL_ATTRIBUTES = {
 export const JAVASCRIPT_CLIENT_ENGINE = 'javascript-sdk';
 export const NODE_CLIENT_ENGINE = 'node-sdk';
 export const REACT_NATIVE_JS_CLIENT_ENGINE = 'react-native-js-sdk';
-export const CLIENT_VERSION = '6.5.1-test.0';
+export const CLIENT_VERSION = '6.5.1-rc.1';
 
 /*
  * Represents the source of a decision for feature management. When a feature
